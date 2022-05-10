2030ba3e7f49b8346cd034539b3194ed36c2b801
Milos Acimovic <macimovic@tenstorrent.com>
2022-05-10 07:14:26 +0000

    Adding debuda smoke test. Just validates that debuda can build and execute.

---
+++ dbd/test/README.md
@@ -0,0 +1,11 @@
+# Debuda tests
+
+Debuda tests are running on silicon.
+
+To run debuda tests locally:
+```
+Build : python3 ci/run.py build --local-run --arch_name grayskull --device_runner silicon --tag push
+Run   : python3 ci/run.py run --local-run --test debuda-tests-grayskull --run_entire_testlist --device_runner silicon
+```
+
+More information about adding test can be found in [CI Test Lists](./../../ci/test-lists/README.md)
--- dbd/test/test-debuda-py.sh
+++ dbd/test/test-debuda-py.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 TMP_OUT_FILE=build/test/dbd-out.tmp
 
-echo make build_hw
-make build_hw
-echo Building verif/op_tests ...
-make verif/op_tests > $TMP_OUT_FILE
+if [ $1 = "skip-build" ]; then
+    echo Skipping build
+else
+    echo make build_hwq
+    make build_hw
+    echo Building verif/op_tests ...
+    make verif/op_tests > $TMP_OUT_FILE
+fi
+
 echo Running op_tests/test_op ...
 ./build/test/verif/op_tests/test_op --netlist verif/op_tests/netlists/netlist_matmul_op_with_fd.yaml --seed 0 --silicon --timeout 500 > $TMP_OUT_FILE
+if [ $? -ne 0 ]; then
+    echo Error in running ./build/test/verif/op_tests/test_op
+    exit 1
+fi
+
 dbd/debuda.py  tt_build/test_op_6142509188972423790 --netlist verif/op_tests/netlists/netlist_matmul_op_with_fd.yaml --commands "s 1 1 24;exit"
+if [ $? -ne 0 ]; then
+    echo Error in running dbd/debuda.py
+    exit 2
+fi
