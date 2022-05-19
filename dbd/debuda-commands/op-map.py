8539655f09a25958414c1cc25a794087c8694b50
Ivan Hamer <ihamer@tenstorrent.com>
2022-05-19 09:30:56 +0000

    Print nicer message for missing buffer

--- dbd/debuda-commands/op-map.py
+++ dbd/debuda-commands/op-map.py
@@ -7,13 +7,12 @@ command_metadata = {
     }
 
 def run(args, context, ui_state = None):
-    rows = []
     navigation_suggestions = []
 
+    rows = []
     for graph_name, graph in context.netlist.graphs.items():
         for op_name in graph.op_names():
             op = graph.root[op_name]
-            print (f"{graph_name}/{op_name}: grid_loc: {op['grid_loc']}, grid_size: {op['grid_size']} ")
             row = [ f"{graph_name}/{op_name}", f"{op['grid_loc']}", f"{op['grid_size']}"]
             rows.append (row)
 
