677eb10a1a1011c704c08ab3f83f5b16384488b7
Milos Acimovic <macimovic@tenstorrent.com>
2022-05-20 09:28:21 +0000

    Debuda: Adding to tapout more information about failed operations

--- dbd/tapout.sh
+++ dbd/tapout.sh
@@ -7,6 +7,7 @@ fi
 TMP_OUT_DIR=debuda_test
 mkdir -p $TMP_OUT_DIR
 
+TMP_FAILED_OP_FILE=$TMP_OUT_DIR'/tapout_failed_ops.log'
 export ARCH_NAME="grayskull"
 
 netlist_filename=$(echo $1 | sed -e 's/.*\(--netlist[ ]*[^ ]*\).*$/\1/' | awk '{print $2}')
@@ -32,6 +33,7 @@ then
 
     printf "Log is stored in $modified_netlist_filename.log\n"
 else
+    echo "Failed operations" > $TMP_FAILED_OP_FILE
     i=0
     ./build/bin/dbd_modify_netlist --netlist $netlist_filename | grep "DBG_" | while read line
     do
@@ -41,9 +43,10 @@ else
 
         # create filename for new netlist
         modified_netlist_filename=$TMP_OUT_DIR'/'$i'_'$base_filename
-
+        test_log=$modified_netlist_filename'.log'
         # log operation name that is tapped out
-        echo $line | awk '{print $1}'
+        queue_name=$(echo $line | awk '{print substr($1, 1, length($1)-1)}')
+        echo $queue_name
 
         # create new netlist file with added output queue for one operation
         sed "/queues:.*/a \ \ $line" $netlist_filename > $modified_netlist_filename
@@ -53,11 +56,16 @@ else
         echo $command
 
         # run new test command and save log
-        $command > $modified_netlist_filename'.log'
+        $command > $test_log
 
         if [ "$?" -ne 0 ]
         then
-            echo "FAILED! Check log file $modified_netlist_filename.log"
+            echo "FAILED! Check log file $test_log"
+            cat $test_log | grep Error | grep $queue_name
+            if [ "$?" -eq 0 ]
+            then
+                echo $queue_name '"'$command'"' $test_log >> $TMP_FAILED_OP_FILE
+            fi
         else
             echo "PASSED"
         fi
