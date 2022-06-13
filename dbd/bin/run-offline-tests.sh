a8206533caf2e24e8701d9318d598e8f4be5a031
Ivan Hamer <ihamer@tenstorrent.com>
2022-06-13 13:29:35 +0000

    Add run-offline-tests.sh

---
+++ dbd/bin/run-offline-tests.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+RUN_DIR=dbd/run           # Where to store run results
+EXPECTED_COVER_PCT=70     # Complain when coverage is below this percantage
+COVERAGE_RUN=""
+# DEBUDA_CMD="dbd/debuda.py --server-cache off"     # Run without reading or writing the cache file
+DEBUDA_CMD="dbd/debuda.py --server-cache on" # Read cache, run, save cache
+# DEBUDA_CMD="dbd/debuda.py --server-cache on"      # Do not read cache, run, save cache
+# set -x # To show all commands run
+set -o pipefail # Propagate exit codes through pipes
+
+if [ "$ARCH_NAME" != "wormhole" ] && [ "$ARCH_NAME" != "grayskull" ]; then
+    echo "ARCH_NAME '$ARCH_NAME' is not valid"
+    exit 1
+fi
+
+# Checks whether Debuda run failed
+check_logfile_exit_code () {
+    log_file="$1"
+    DBD_EXIT_CODE=`cat $log_file | grep "Exiting with code " | awk '{print $4}'`
+    if [ "$DBD_EXIT_CODE" == "0" ]; then echo "Debuda run OK"; else
+        echo "FAIL: Debuda run failed with exit code '$DBD_EXIT_CODE' (see $log_file)";
+        exit $DBD_EXIT_CODE
+    fi
+}
+
+# Run one coverage command
+coverage_run () {
+    log_file=$1
+    cov_command="$2"
+    cov_args="$3"
+    debuda_cmd="$4"
+    debuda_commands_arg="$6"
+    $cov_command $cov_args $debuda_cmd "$5" "$debuda_commands_arg" | tee "$log_file"
+    check_logfile_exit_code "$log_file"
+}
+
+ROOT=`pwd`
+
+# Run Debuda
+
+## This debuda run command sequence is supposed to return exact same text always
+rm -rf $RUN_DIR/dbd-export
+unzip dbd/test/exported/$ARCH_NAME/coverage-exact-match.zip -d $RUN_DIR/dbd-export
+cd $ROOT/$RUN_DIR/dbd-export
+coverage_run coverage-exact-match.log "$COVERAGE_RUN" "" "../../../$DEBUDA_CMD" --commands "hq; dq; abs; abs 1; s 1 1 8; cdr 1 1; c 1 1; d; t 1 0; d 0; d 1; cdr 1 1; b 10000030000; 0; 0; eq; brxy 1 1 0 0; help; x"
+cd $ROOT
+
+# Compare the output with the expected
+compare_files="$ROOT/$RUN_DIR/dbd-export/coverage-exact-match.log $ROOT/dbd/test/expected-results/coverage-$ARCH_NAME-exact-match.expected"
+echo diff $compare_files
+diff $compare_files
+compare_exit_code="$?"
+if [ "$compare_exit_code" == "0" ]; then echo PASS; else echo FAIL; exit "$compare_exit_code"; fi
+
+# ## This command might read stuff that is not always the same
+# rm -rf $RUN_DIR/dbd-export
+# unzip dbd/test/exported/$ARCH_NAME/coverage-fuzzy-match.zip -d $RUN_DIR/dbd-export
+# cd $ROOT/$RUN_DIR/dbd-export
+# coverage_run coverage-fuzzy-match.log "$COVERAGE_RUN" "" "../../../$DEBUDA_CMD" --commands "srs 0; srs 1; srs 2; fd; t 1 1; op-map; x"
+# cd $ROOT
