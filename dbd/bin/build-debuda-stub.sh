e6dd0c5cec543482f32988a4a1816cb9a211f9ed
Ivan Hamer <ihamer@tenstorrent.com>
2022-07-12 06:51:38 +0000

    Debuda-stub: remove mem_tlb_mutex on start.

--- dbd/bin/build-debuda-stub.sh
+++ dbd/bin/build-debuda-stub.sh
@@ -8,7 +8,7 @@ make device
 make model
 
 echo Compiling $TARGET_EXE
-g++ -DFMT_HEADER_ONLY  -Ithird_party/fmt/ -Isrc/firmware/riscv/grayskull/ -Icommon/model -Idevice/grayskull/ -Inetlist -Imodel -I. -Icore_graph_lib device/tt_silicon_driver_debuda_stub.cpp -std=gnu++17 -Isrc/firmware/riscv/ -Iversim/grayskull/headers/vendor/yaml-cpp/include ./device/lib/libyaml-cpp.a ./build/lib/libmodel.a ./build/lib/libdevice.so -lzmq -o $TARGET_EXE
+g++ -DFMT_HEADER_ONLY  -Ithird_party/fmt/ -Isrc/firmware/riscv/grayskull/ -Icommon/model -Idevice/grayskull/ -Inetlist -Imodel -I. -Icore_graph_lib device/tt_silicon_driver_debuda_stub.cpp -std=gnu++17 -Isrc/firmware/riscv/ -Iversim/grayskull/headers/vendor/yaml-cpp/include ./device/lib/libyaml-cpp.a ./build/lib/libmodel.a ./build/lib/libdevice.so -lzmq -lpthread -o $TARGET_EXE
 
 echo To run debuda_stub enter: $TARGET_EXE
 
