febb8475305221e3cb748b2ce6fe43b0251c2ca0
Umair Cheema <ucheema@tenstorrent.com>
2022-07-18 22:30:48 +0000

    [run_ci] Revert 37a1520b5f54c00543a767a827b11fa99f0a3c27. Will commit the change with proper erisc spirom fw update.

--- dbd/debuda.py
+++ dbd/debuda.py
@@ -182,7 +182,7 @@ def print_epoch_queue_summary (cmd, context, ui_state):
     EPOCH_Q_NUM_SLOTS = 32
     epoch0_start_table_size_bytes = GridSizeRow*GridSizeCol*(EPOCH_Q_NUM_SLOTS*2+8)*4
     # DRAM_CHANNEL_CAPACITY_BYTES  = 1024 * 1024 * 1024
-    DRAM_PERF_SCRATCH_SIZE_BYTES =   8 * 1024 * 1024
+    DRAM_PERF_SCRATCH_SIZE_BYTES =   40 * 1024 * 1024
     # DRAM_HOST_MMIO_SIZE_BYTES    =  256 * 1024 * 1024
     reserved_size_bytes = DRAM_PERF_SCRATCH_SIZE_BYTES - epoch0_start_table_size_bytes
 
