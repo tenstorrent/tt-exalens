1fbdf5541a5699f54dfba27898c12c4ab24fe8c0
Milos Acimovic <macimovic@tenstorrent.com>
2022-07-26 15:17:42 +0000

    Changing dbd/bin/build-debuda-stub.sh to use make dbd/debuda-stub

--- dbd/bin/build-debuda-stub.sh
+++ dbd/bin/build-debuda-stub.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 # Minimal compile and test for silicon driver
 set -e
-TARGET_EXE=dbd/debuda-stub
-
-echo Compiling dependencies
-make device
-make model
+TARGET_EXE=build/bin/debuda-stub
 
 echo Compiling $TARGET_EXE
-g++ -DFMT_HEADER_ONLY  -Ithird_party/fmt/ -Isrc/firmware/riscv/grayskull/ -Icommon/model -Idevice/grayskull/ -Inetlist -Imodel -I. -Icore_graph_lib device/tt_silicon_driver_debuda_stub.cpp -std=gnu++17 -Isrc/firmware/riscv/ -Iversim/grayskull/headers/vendor/yaml-cpp/include ./device/lib/libyaml-cpp.a ./build/lib/libmodel.a ./build/lib/libdevice.so -lzmq -lpthread -o $TARGET_EXE
-
+make dbd/debuda-stub
 echo To run debuda_stub enter: $TARGET_EXE
-
-# TT_PCI_LOG_LEVEL=1 $TARGET_EXE
-
-# Copy debuda-stub to build directory
-mkdir -p build
-mkdir -p build/bin
-cp dbd/debuda-stub build/bin/debuda-stub
