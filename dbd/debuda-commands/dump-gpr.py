0564c8f62569f618bf8c362b2268b1741027d977
Milos Acimovic <macimovic@tenstorrent.com>
2022-08-26 08:13:13 +0000

    Macimovic/riscv debugging

---
+++ dbd/debuda-commands/dump-gpr.py
@@ -0,0 +1,42 @@
+# Prints all RISC-V registers for TRISC0, TRISC1, TRISC2 and Brisc on current core.
+# If cannot pause(halt) core, it will not write anyhting. If core is not active, currently it throws exception.
+import tabulate
+from tt_debug_risc import RiscDebug, RiscLoc
+
+command_metadata = {
+    "short" : "gpr",
+    "expected_argument_count" : 0,
+    "arguments_description" : ": dumps risc general purpose registers"
+}
+RISC_REGS = {
+    0:'zero', 1:'ra', 2:'sp', 3:'gp', 4:'tp', 5:'t0', 6:'t1', 7:'t2', 8:'s0 / fp', 9:'s1', 10:'a0', 11:'a1', 12:'a2',
+    13:'a3', 14:'a4', 15:'a5', 16:'a6', 17:'a7', 18:'s2', 19:'s3', 20:'s4', 21:'s5', 22:'s6', 23:'s7', 24:'s8', 25:'s9',
+    26:'s10', 27:'s11', 28:'t3', 29:'t4', 30:'t5', 31:'t6', 32:'PC'}
+
+def run(args, context, ui_state = None):
+    result = {}
+    device_id = ui_state['current_device_id']
+    x = ui_state['current_x']
+    y = ui_state['current_y']
+    for i in range(0,4):
+        risc = RiscDebug(RiscLoc(device_id, x, y, 0, i))
+        risc.enable_debug()
+        risc.pause()
+        if risc.is_paused():
+            result[i]={}
+            for j in range(0,33):
+                reg_val = risc.read_gpr(j)
+                result[i][j] = reg_val
+            risc.contnue()
+
+
+    table=[]
+    for i in range(0,33):
+        row = [f"{i} - {RISC_REGS[i]}"]
+        for j in range(0,4):
+            row.append(f"0x{result[j][i]:08x}" if j in result else '-')
+        table.append(row)
+    if len(table) > 0:
+        print (tabulate.tabulate(table, headers=["Register", "Brisc", "Trisc0", "Trisc1", "Trisc2" ] ))
+
+    return []
