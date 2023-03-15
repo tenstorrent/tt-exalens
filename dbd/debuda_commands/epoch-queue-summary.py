52e23de9401874f685740ac0cfed109981d0da8e
Djordje Maksimovic <dmaksimovic@tenstorrent.com>
2023-03-15 20:55:59 +0000

    FW/Runtime - Epoch Queues back to DRAM with Shadow Header in L1 (Issue #914)
     FW Feature from Djordje with some notes from Kyle:
    
       Updated ncrisc and erisc to start polling from dram when l1 pointers are updated
    
       - Add back mailbox flag 0xF after ncrisc l1 epoch queue initialized to tell runtime it's ready.
       - Revert erisc_main.cc change - host/runtime will initialize erisc l1 epoch queue
    
     Runtime Changes:
    
     - Runtime writes epoch cmd queue wrptr to L1 now too.
    
     - This avoids L1/DRAM Race exposed by bert_large multichip training
       with -O4 where EpochCmdIOQueueUpdate cmd arrived in L1 before it's
       external blob in DRAM even though runtime intended to write them in
       correct order. Talked about in https://yyz-gitlab.local.tenstorrent.com/tenstorrent/budabackend/-/issues/1028#note_191181
    
     - Original issue for epoch-queue-in-l1: https://yyz-gitlab.local.tenstorrent.com/tenstorrent/budabackend/-/issues/914
       updated to reflect decision to revert back to DRAM, but keep benefits
       (reduce DRAM incessant polling from ncrisc) of epoch-queue-in-L1
    
     - No more TT_BACKEND_EPOCH_QUEUE_IN_DRAM parsing, it's always in DRAM now
    
     - Update dbd for DRAM only, and to parse epoch_queue_num_slots which is
       useful for experiments where we modify number of slots.
    
     - Small perf bump on avg from 561 Samples/sec -> 563 Samples/sec without and with
       l1-shadow feature using 8-chip bert large training in GS.
    
     - Small perf drop in dram read/write perf tests of 0.3GB/s, going to
       re-capture perf target on subsequent submit to avoid CI fails.

--- dbd/debuda_commands/epoch-queue-summary.py
+++ dbd/debuda_commands/epoch-queue-summary.py
@@ -19,14 +19,13 @@ def run (cmd, context, ui_state):
     device_id = context.netlist.graph_name_to_device_id(graph_name)
     epoch_device = context.devices[device_id]
     distribtued_eq = bool(runtime_data.root.get("distribute_epoch_tables", 1))
-    epoch_queue_in_dram = bool(runtime_data.root.get("epoch_queue_in_dram", 1))
+    EPOCH_Q_NUM_SLOTS = runtime_data.root.get('epoch_queue_num_slots', 64)
 
-    print (f"{util.CLR_INFO}Epoch queues for graph {graph_name}, device id {device_id}{util.CLR_END}")
+    print (f"{util.CLR_INFO}Epoch queues for graph {graph_name}, device id {device_id} with EPOCH_Q_NUM_SLOTS: {EPOCH_Q_NUM_SLOTS} {util.CLR_END}")
 
     # From tt_epoch_dram_manager::tt_epoch_dram_manager and following the constants
     GridSizeRow = 16
     GridSizeCol = 16
-    EPOCH_Q_NUM_SLOTS = 64
     EPOCH_Q_SLOT_SIZE = 32
     EPOCH_Q_SLOTS_OFFSET = 32
     epoch0_start_table_size_bytes = GridSizeRow*GridSizeCol*(EPOCH_Q_NUM_SLOTS*EPOCH_Q_SLOT_SIZE+EPOCH_Q_SLOTS_OFFSET)
@@ -46,19 +45,14 @@ def run (cmd, context, ui_state):
     for blocktype in ["functional_workers"]: # , "eth"]:
         for loc in epoch_device.get_block_locations (block_type = blocktype):
             x, y = loc[0], loc[1]
-            loc_is_dram = epoch_queue_in_dram or blocktype == "eth"
             coretype = "Worker" if blocktype == "functional_workers" else "Ethernet"
-            if loc_is_dram:
-                loc_str = "DRAM"
-                if distribtued_eq:
-                    dram_loc = epoch_device.get_t6_queue_location (loc) # FIXME - Hardcoded for GS, wrong for WH "eth" cores.
-                EPOCH_QUEUE_START_ADDR = reserved_size_bytes
-                offset = (GridSizeCol * y + x) * (EPOCH_Q_NUM_SLOTS*EPOCH_Q_SLOT_SIZE+EPOCH_Q_SLOTS_OFFSET)
-                addr = EPOCH_QUEUE_START_ADDR + offset
-                x, y = dram_loc[0], dram_loc[1] # Override core xy for reading.
-            else:
-                loc_str = "L1"
-                addr = runtime_data.root['ncrisc_l1_epoch_q_base'] # runtime_data.root['eth_l1_epoch_q_base'] when erisc moves to L1.
+            loc_str = "DRAM"
+            if distribtued_eq:
+                dram_loc = epoch_device.get_t6_queue_location (loc) # FIXME - Hardcoded for GS, wrong for WH "eth" cores.
+            EPOCH_QUEUE_START_ADDR = reserved_size_bytes
+            offset = (GridSizeCol * y + x) * (EPOCH_Q_NUM_SLOTS*EPOCH_Q_SLOT_SIZE+EPOCH_Q_SLOTS_OFFSET)
+            addr = EPOCH_QUEUE_START_ADDR + offset
+            x, y = dram_loc[0], dram_loc[1] # Override core xy for reading.
 
             rdptr = tt_device.SERVER_IFC.pci_read_xy (device_id, x, y, 0, addr)
             wrptr = tt_device.SERVER_IFC.pci_read_xy (device_id, x, y, 0, addr + 4)
