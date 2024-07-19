7b56a549ba4b12e36dc91db2718bd88dcb6b2f0b
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2024-07-19 09:53:18 +0000

    Adding test_halt_continue

--- dbd/tests/test_lib.py
+++ dbd/tests/test_lib.py
@@ -205,8 +205,8 @@ class TestRunElf(unittest.TestCase):
 		lib.write_word_to_device(core_loc, 4, 0x876545B7, context=self.context)
 		# Store the word value from register x11 to address from register x10 (sw x11, 0(x10))
 		lib.write_word_to_device(core_loc, 8, 0x00B52023, context=self.context)
-		# Infinite loop (jal 12)
-		lib.write_word_to_device(core_loc, 12, RiscLoader.get_jump_to_offset_instruction(12), context=self.context)
+		# Infinite loop (jal 0)
+		lib.write_word_to_device(core_loc, 12, RiscLoader.get_jump_to_offset_instruction(0), context=self.context)
 
 		# Take risc out of reset
 		rdbg.set_reset_signal(False)
@@ -420,8 +420,8 @@ class TestDebugging(unittest.TestCase):
 		lib.write_word_to_device(core_loc, 8, 0x876545B7, context=self.context)
 		# Store the word value from register x11 to address from register x10 (sw x11, 0(x10))
 		lib.write_word_to_device(core_loc, 12, 0x00B52023, context=self.context)
-		# Infinite loop (jal 12)
-		lib.write_word_to_device(core_loc, 16, RiscLoader.get_jump_to_offset_instruction(16), context=self.context)
+		# Infinite loop (jal 0)
+		lib.write_word_to_device(core_loc, 16, RiscLoader.get_jump_to_offset_instruction(0), context=self.context)
 
 		# Take risc out of reset
 		rdbg.set_reset_signal(False)
@@ -460,8 +460,8 @@ class TestDebugging(unittest.TestCase):
 		lib.write_word_to_device(core_loc, 8, 0x876545B7, context=self.context)
 		# Store the word value from register x11 to address from register x10 (sw x11, 0(x10))
 		lib.write_word_to_device(core_loc, 12, 0x00B52023, context=self.context)
-		# Infinite loop (jal 12)
-		lib.write_word_to_device(core_loc, 16, RiscLoader.get_jump_to_offset_instruction(16), context=self.context)
+		# Infinite loop (jal 0)
+		lib.write_word_to_device(core_loc, 16, RiscLoader.get_jump_to_offset_instruction(0), context=self.context)
 
 		# Take risc out of reset
 		rdbg.set_reset_signal(False)
@@ -474,6 +474,75 @@ class TestDebugging(unittest.TestCase):
 		ret = lib.read_words_from_device(core_loc, addr, context=self.context)
 		# Value should not be changed and should stay the same since core is in halt
 		self.assertEqual(ret[0], 0x87654000)
+
+		# Stop risc with reset
+		rdbg.set_reset_signal(True)
+
+	def test_halt_continue(self, happy: bool = True):
+		"""Test running 20 bytes of generated code that just write data on memory and does infinite loop. All that is done on brisc."""
+		core_loc = "0,0"
+		addr = 0x10000
+
+		loc = OnChipCoordinate.create(core_loc, device=self.context.devices[0])
+		rloc = RiscLoc(loc, 0, 0)
+		rdbg = RiscDebug(rloc, self.context.server_ifc)
+
+		# Stop risc with reset
+		rdbg.set_reset_signal(True)
+
+		# Write our data to memory
+		lib.write_word_to_device(core_loc, addr, 0x12345678, context=self.context)
+		ret = lib.read_words_from_device(core_loc, addr, context=self.context)
+		self.assertEqual(ret[0], 0x12345678)
+
+		# Write code for brisc core at address 0
+		# ebreak
+		lib.write_word_to_device(core_loc, 0, 0x00100073, context=self.context)
+		# Load Immediate Address 0x10000 into x10 (lui x10, 0x10)
+		lib.write_word_to_device(core_loc, 4, 0x00010537, context=self.context)
+		# Load Immediate Value 0x87654000 into x11 (lui x11, 0x87654)
+		lib.write_word_to_device(core_loc, 8, 0x876545B7, context=self.context)
+		# Store the word value from register x11 to address from register x10 (sw x11, 0(x10))
+		lib.write_word_to_device(core_loc, 12, 0x00B52023, context=self.context)
+		# Increment x11 by 1 (addi x11, x11, 1)
+		lib.write_word_to_device(core_loc, 16, 0x00158593, context=self.context)
+		# Store the word value from register x11 to address from register x10 (sw x11, 0(x10))
+		lib.write_word_to_device(core_loc, 20, 0x00B52023, context=self.context)
+		# Infinite loop (jal -8)
+		lib.write_word_to_device(core_loc, 24, RiscLoader.get_jump_to_offset_instruction(-8), context=self.context)
+
+		# Take risc out of reset
+		rdbg.set_reset_signal(False)
+
+		# Verify that value didn't change cause of ebreak
+		ret = lib.read_words_from_device(core_loc, addr, context=self.context)
+		self.assertEqual(ret[0], 0x12345678)
+
+		# Continue
+		rdbg.enable_debug()
+		rdbg.cont()
+
+		# Verify that value changed cause of continue
+		ret = lib.read_words_from_device(core_loc, addr, context=self.context)
+		self.assertGreaterEqual(ret[0], 0x87654000)
+		previous_value = ret[0]
+
+		# Loop halt and continue
+		for i in range(10):
+			# Halt
+			rdbg.halt()
+
+			# Read value
+			ret = lib.read_words_from_device(core_loc, addr, context=self.context)
+			self.assertGreater(ret[0], previous_value)
+			previous_value = ret[0]
+
+			# Second read should have the same value if core is halted
+			ret = lib.read_words_from_device(core_loc, addr, context=self.context)
+			self.assertEqual(ret[0], previous_value)
+
+			# Continue
+			rdbg.cont()
 
 		# Stop risc with reset
 		rdbg.set_reset_signal(True)
