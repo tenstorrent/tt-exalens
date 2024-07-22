e3c1a71ae2bde29e0566ad2110ae7817fb66692d
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-07-22 14:13:30 +0000

    - updated dependencies to be relative

--- dbd/server/app/module.mk
+++ dbd/server/app/module.mk
@@ -2,8 +2,6 @@
 DEBUDA_SERVER_SRCS = $(wildcard dbd/server/app/*.cpp)
 DEBUDA_SERVER = $(BINDIR)/debuda-server-standalone
 
-# Libraries this target depends on
-DEBUDA_SERVER_LINK_DEPS = $(DEBUDA_SERVER_LIB) $(BACKEND_LIB)
 # Also add those libraries as with shared name to avoid hardcoding library paths.
 DEBUDA_SERVER_LIB_DEPS = -ldbdserver -ldevice -lstdc++fs
 
@@ -15,15 +13,15 @@ DEBUDA_SERVER_INCLUDES = \
 	-I$(DEBUDA_HOME)/third_party/umd \
 	-Idbd/server/lib/inc \
 
-DEBUDA_SERVER_LDFLAGS =  $(DEBUDA_SERVER_LIB_DEPS) -lyaml-cpp -lzmq -Wl,-rpath,\$$ORIGIN/../lib:\$$ORIGIN -pthread
+DEBUDA_SERVER_LDFLAGS = -Wl,-rpath,'$$ORIGIN/../lib:$$ORIGIN' -pthread $(DEBUDA_SERVER_LIB_DEPS) -lyaml-cpp -lzmq 
 
 -include $(DEBUDA_SERVER_DEPS)
 
 dbd/server/app: $(DEBUDA_SERVER)
 
-$(DEBUDA_SERVER): $(DEBUDA_SERVER_OBJS) $(DEBUDA_SERVER_LINK_DEPS) $(UMD_DEVICE_LIB) $(DEBUDA_SERVER_LIB)
+$(DEBUDA_SERVER): $(DEBUDA_SERVER_OBJS) $(UMD_DEVICE_LIB) $(DEBUDA_SERVER_LIB)
 	@mkdir -p $(@D)
-	$(CXX) $(CFLAGS) $(CXXFLAGS) $(DEBUDA_SERVER_LIB_DEPS) -o $@ $^ $(LDFLAGS) $(DEBUDA_SERVER_LDFLAGS)
+	$(CXX) $(CFLAGS) $(CXXFLAGS) $(DEBUDA_SERVER_LIB_DEPS) -o $@ $(DEBUDA_SERVER_OBJS) $(LDFLAGS) $(DEBUDA_SERVER_LDFLAGS)
 
 .PRECIOUS: $(OBJDIR)/dbd/server/app/%.o
 $(OBJDIR)/dbd/server/app/%.o: dbd/server/app/%.cpp
