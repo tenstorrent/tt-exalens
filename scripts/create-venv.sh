371bcdef872c5488b0b4f935c6ef5693c17bfb42
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-07-22 11:46:23 +0000

    - pipeline update - updated dependencies to be the same on all Ubuntu versions - updated build to avoid taring files - static checks are now an independent workflow running independently - updated venv creation to not include dependencies, as it is used in multiple contexts

--- scripts/create-venv.sh
+++ scripts/create-venv.sh
@@ -10,12 +10,6 @@ python3 -m venv $PYTHON_ENV_DIR
 
 source $PYTHON_ENV_DIR/bin/activate
 
-echo "Updating pip"
-python3 -m pip install --upgrade pip
-
-echo "Setting up virtual env"
-python3 -m pip install setuptools wheel
-
-echo "Installing dev dependencies"
-python3 -m pip install -r $(pwd)/dbd/requirements.txt
-python3 -m pip install -r $(pwd)/dbd/tests/test_requirements.txt
+echo "Ensuring pip is installed and up-to-date"
+python3 -m ensurepip
+pip install --upgrade pip
