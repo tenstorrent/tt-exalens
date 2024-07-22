73460832fa02935569f73eb15a98124ee0cd7901
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-07-22 11:42:31 +0000

    - chore: updated run-wheel script to remove unneeded code

--- test/wheel/run-wheel.sh
+++ test/wheel/run-wheel.sh
@@ -4,21 +4,10 @@
 set -e
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUTPUT_DIR=
 
 source ${THIS_SCRIPT_DIR}/../test-base.sh
 
-# set default value for TIMEOUT
-if [ -z "$TEST_RUN_TIMEOUT" ]; then TEST_RUN_TIMEOUT=500; fi
-echo -e "${YELLOW}TEST_RUN_TIMEOUT=$TEST_RUN_TIMEOUT${NC}"
-
-mkdir -p build/test
-TMP_OUT_FILE=$(realpath build/test/dbd-out.tmp)
-echo -e "${YELLOW}TMP_OUT_FILE=$TMP_OUT_FILE${NC}"
-
-if [ "$1" = "skip-build" ]; then
-    echo Skipping build used for CI and tests
-elif [ -z "${DEBUDA_HOME}" ]; then
+if [ -z "${DEBUDA_HOME}" ]; then
     echo -e "${RED}Error:${NC} DEBUDA_HOME is not set. Please set DEBUDA_HOME to the root of the budabackend repository"
     exit 1
 fi
@@ -27,8 +16,6 @@ fi
 # Running tests
 
 DEBUDA_COMMANDS='h;wxy 0,0 0 0x123;brxy 0,0 0 64;cdr;gpr;d 0 netlist nocVirt;x'
-
-RUN_OUTPUT_DIR=${BASE_TEST_DIR}/debuda_test
 
 echo -e "${YELLOW}INFO: ${NC}Running simple debuda commands ..."
 debuda --test --commands="$DEBUDA_COMMANDS"
