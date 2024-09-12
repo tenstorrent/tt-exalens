0efd8dd761897bfc4463c19d9a2d1dd76618e3a7
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-09-12 12:55:16 +0000

    - update install-deps.sh to use pip directly - remove venv creation from test workflow

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,15 +7,15 @@ WHEEL_INSTALL=${WHEEL_INSTALL:-}
 
 if [ -n "$DBD_INSTALL" ]; then
     echo "Installing dbd dependencies..."
-    python -m pip install -r dbd/requirements.txt
+    pip install -r dbd/requirements.txt
 fi
 
 if [ -n "$TEST_INSTALL" ]; then
     echo "Installing test dependencies..."
-    python -m pip install -r test/test_requirements.txt
+    pip install -r test/test_requirements.txt
 fi
 
 if [ -n "$WHEEL_INSTALL" ] then
     echo "Installing wheel dependencies..."
-    python -m pip install -r wheel build setuptools
+    pip install -r wheel build setuptools
 fi
