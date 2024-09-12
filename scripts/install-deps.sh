b5e9dc6e9eb5836a096ac031f7b84cf755d7f9bc
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-09-12 10:36:23 +0000

    - try finding pip

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,15 +7,15 @@ WHEEL_INSTALL=${WHEEL_INSTALL:-}
 
 if [ -n "$DBD_INSTALL" ]; then
     echo "Installing dbd dependencies..."
-    pip install -r dbd/requirements.txt
+    python -m pip install -r dbd/requirements.txt
 fi
 
 if [ -n "$TEST_INSTALL" ]; then
     echo "Installing test dependencies..."
-    pip install -r test/test_requirements.txt
+    python -m pip install -r test/test_requirements.txt
 fi
 
 if [ -n "$WHEEL_INSTALL" ] then
     echo "Installing wheel dependencies..."
-    pip install -r wheel build setuptools
+    python -m pip install -r wheel build setuptools
 fi
