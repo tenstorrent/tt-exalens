0f2ff85a104c1bba86d41254fad23c68a626cf5e
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-09-12 09:36:27 +0000

    all-tests.yaml: - chmod doesn't use sudo anymore - venv is sourced in every subshell now setup-python-venv: - optional installation of dependencies is now included create-venv.sh: - optional installation of dependencies is now included Docker.debuda: - removed superfluous library installations

--- scripts/create-venv.sh
+++ scripts/create-venv.sh
@@ -5,6 +5,10 @@ if [ -z "$PYTHON_ENV_DIR" ]; then
     PYTHON_ENV_DIR="$(pwd)/.venv"
 fi
 
+DBD_INSTALL=${DBD_INSTALL:-}
+TEST_INSTALL=${TEST_INSTALL:-}
+WHEEL_INSTALL=${WHEEL_INSTALL:-}
+
 echo "Creating virtual env in: $PYTHON_ENV_DIR"
 python3 -m venv $PYTHON_ENV_DIR
 
@@ -13,3 +17,18 @@ source $PYTHON_ENV_DIR/bin/activate
 echo "Ensuring pip is installed and up-to-date"
 python3 -m ensurepip
 pip install --upgrade pip
+
+if [ -n "$DBD_INSTALL" ]; then
+    echo "Installing dbd dependencies..."
+    pip install -r dbd/requirements.txt
+fi
+
+if [ -n "$TEST_INSTALL" ]; then
+    echo "Installing test dependencies..."
+    pip install -r test/test_requirements.txt
+fi
+
+if [ -n "$WHEEL_INSTALL" ] then
+    echo "Installing wheel dependencies..."
+    pip install -r wheel build setuptools
+fi
