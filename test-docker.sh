7579a97482fbedaace7eb0b3bb60e1dbd31c3786
Dusan Cvijetic <dcvijetic@tenstorrent.com>
2024-09-12 13:20:41 +0000

    - remove junk

--- test-docker.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --device /dev/tenstorrent/0 \
-	-v /dev/hugepages:/dev/hugepages \
-	-v /dev/hugepages-1G:/dev/hugepages-1G \
-	-v /etc/udev/rules.d:/etc/udev/rules.d \
-	-v /lib/modules:/lib/modules \
-	-v /localdev/dcvijetic:/localdev/dcvijetic \
-	debuda-docker-ci bash
