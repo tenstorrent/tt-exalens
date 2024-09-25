b2e423a242ead5f8fae4e4eac3b7872fa47d930c
Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>
2024-09-25 11:00:25 +0200

    Reducing build requirements when building wheel (#122)
    Previously, we built whole repository. Now we build minimal set of
    projects...

--- setup.py
+++ setup.py
@@ -75,8 +75,8 @@ class MyBuild(build_ext):
     def _call_build(self):
         env = os.environ.copy()
         nproc = os.cpu_count()
-        print(f"make -j{nproc} build")
-        subprocess.check_call([f"cd {debuda_home} && make -j{nproc} build"], env=env, shell=True)
+        print(f"make -j{nproc} dbd/server dbd/pybind")
+        subprocess.check_call([f"cd {debuda_home} && make -j{nproc} dbd/server dbd/pybind"], env=env, shell=True)
 
     def _copy_files(self, target_path):
         strip_symbols = os.environ.get("STRIP_SYMBOLS", "0") == "1"
