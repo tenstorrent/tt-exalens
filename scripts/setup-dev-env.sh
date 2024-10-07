a63e776e27ca9036be3458b2de9228b46f75b306
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2024-10-07 11:42:25 +0000

    Adding more packages to install for dev environment

--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install rsync gdb libyaml-cpp-dev libzmq3-dev -y
+sudo apt-get install rsync gdb libyaml-cpp-dev libzmq3-dev libboost-all-dev -y
 pip install -r dbd/requirements.txt
 pip install -r test/test_requirements.txt
