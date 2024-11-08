2f45a24504c7ec62df346c1b738cb3561e516955
lgojic-tt <lgojic@tenstorrent.com>
2024-11-08 12:06:49 +0000

    Debugging CI for grayskull

--- test/dbd/unit_tests/test_lib.py
+++ test/dbd/unit_tests/test_lib.py
@@ -470,5 +470,5 @@ class TestARC(unittest.TestCase):
             arc_core_loc = device.get_arc_block_location()
 
             scratch2 = arc_read(self.context, device_id, arc_core_loc, device.get_register_addr("ARC_RESET_SCRATCH2"))
-
+            print(hex(scratch2))
             assert(scratch2 == 0xbebaceca)
