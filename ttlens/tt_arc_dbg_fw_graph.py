db78dfd25ad1a520f2e5c062a169156bf3c95946
lgojic-tt <lgojic@tenstorrent.com>
2024-12-12 15:31:58 +0000

    Adding options to graph command

--- ttlens/tt_arc_dbg_fw_graph.py
+++ ttlens/tt_arc_dbg_fw_graph.py
@@ -17,27 +17,14 @@ from ttlens.tt_arc_dbg_fw import (
 from ttlens.tt_lens_lib import read_from_device
 import csv
 
-def graph_current(log_context: ArcDfwLogContext ,device_id: int = 0, context: Context = None) -> None:
-    load_arc_dbg_fw(log_context=log_context, device_id=device_id, context=context)
-    buffer_data = arc_dfw_get_logs(log_context,device_id,context)
-    save_to_csv(buffer_data, "fw/")
-    plot_from_csv("fw/log_data.csv")
-
-def format_output(value, output_type):
-    if output_type == 'int':
-        return int(value)
-    elif output_type == 'float':
-        return struct.unpack('<f', struct.pack('<I', value))[0]
-    elif output_type == 'float_div_16': # special case for temperature data
-        return struct.unpack('<f', struct.pack('<I', value))[0] / 16
-    elif output_type == 'hex':
-        return value
-    else:
-        return value
+def arc_get_logs_from_list(log_names: List[str], device_id: int = 0, context: Context = None) -> None:
+    log_context = ArcDfwLogContextFromList(log_names)
+    buffer_data = arc_dfw_get_logs(log_context, device_id, context)
+    return buffer_data
 
 def save_to_csv(log_data: bytes, save_location: str) -> None:
-    csv_file_path = os.path.join(save_location, "log_data.csv")
-    with open(csv_file_path, mode='w', newline='') as csv_file:
+    
+    with open(save_location, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         headers = ["Sample"] + list(log_data.keys())
         writer.writerow(headers)
@@ -45,21 +32,31 @@ def save_to_csv(log_data: bytes, save_location: str) -> None:
             row = [i] + [log_data[log_name][i] for log_name in log_data]
             writer.writerow(row)
 
-def plot_log_data(log_data: bytes, save_location: str) -> None:
-    import matplotlib.pyplot as plt
+# def plot_log_data(log_data: bytes, save_location: str) -> None:
+#     import matplotlib.pyplot as plt
 
-    for log_name, data in log_data.items():
-        plt.figure()
-        plt.plot(data)
-        plt.title(log_name)
-        plt.xlabel('Sample')
-        plt.ylabel('Value')
-        plt.ylim(min(data) - 1, max(data) + 1) 
-        plt.savefig(os.path.join(save_location, f"{log_name}.png"))
-        plt.close()
+#     for log_name, data in log_data.items():
+#         plt.figure()
+#         plt.plot(data)
+#         plt.title(log_name)
+#         plt.xlabel('Sample')
+#         plt.ylabel('Value')
+#         plt.ylim(min(data) - 1, max(data) + 1) 
+#         plt.savefig(os.path.join(save_location, f"{log_name}.png"))
+#         plt.close()
 
 def parse_buffer(buffer: bytes, log_context: ArcDfwLogContext) -> None:
-
+    def format_output(value, output_type):
+        if output_type == 'int':
+            return int(value)
+        elif output_type == 'float':
+            return struct.unpack('<f', struct.pack('<I', value))[0]
+        elif output_type == 'float_div_16': # special case for temperature data
+            return struct.unpack('<f', struct.pack('<I', value))[0] / 16
+        elif output_type == 'hex':
+            return value
+        else:
+            return value
     log_data = {log_info.log_name: [] for log_info in log_context.log_list}
     num_logs = len(log_context.log_list)
 
@@ -88,27 +85,43 @@ def arc_dfw_get_logs( log_context: ArcDfwLogContext = ArcDfwLogContextFromYaml("
 
     arc_dbg_fw_command("start", device_id, context)
 
+    start_time = time.time()
     buffer_size = arc_dbg_fw_get_buffer_size() - len(DFW_BUFFER_HEADER_OFFSETS) * 4
     while (t := read_dfw_buffer_header("num_log_calls", device_id, context)*len(log_context.log_list)*4) <= buffer_size:
         continue
 
+    end_time = time.time() 
+    print(f"Time taken to fill buffer: {end_time - start_time} seconds")
+
     arc_dbg_fw_command("stop", device_id, context)
 
+    start_time = time.time()
     buffer = read_arc_dfw_buffer(device_id, context)
+    end_time = time.time() 
+    print(f"Time taken to fill buffer: {end_time - start_time} seconds")
+
     return parse_buffer(buffer, log_context)
 
-def plot_from_csv(csv_file_path: str) -> None:
+def read_logs_from_csv(csv_file_path: str) -> None:
     import pandas as pd
-    import matplotlib.pyplot as plt
-
     df = pd.read_csv(csv_file_path)
+    log_data = {}
     for column in df.columns[1:]:
-        plt.figure()
-        plt.plot(df['Sample'], df[column])
-        plt.title(column)
-        plt.xlabel('Sample')
-        plt.ylabel('Value')
-        plt.ylim(min(df[column]) - 1, max(df[column]) + 1)
-        plt.show()
-        plt.savefig(os.path.join(os.path.dirname(csv_file_path), f"{column}.png"))
-        plt.close()
+        log_data[column] = df[column].tolist()
+    return log_data
+
+# def plot_from_csv(csv_file_path: str) -> None:
+#     import pandas as pd
+#     import matplotlib.pyplot as plt
+
+#     df = pd.read_csv(csv_file_path)
+#     for column in df.columns[1:]:
+#         plt.figure()
+#         plt.plot(df['Sample'], df[column])
+#         plt.title(column)
+#         plt.xlabel('Sample')
+#         plt.ylabel('Value')
+#         plt.ylim(min(df[column]) - 1, max(df[column]) + 1)
+#         plt.show()
+#         plt.savefig(os.path.join(os.path.dirname(csv_file_path), f"{column}.png"))
+#         plt.close()
--- ttlens/ttlens_commands/arc-dfw-graph.py
+++ ttlens/ttlens_commands/arc-dfw-graph.py
@@ -3,13 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 """
 Usage:
-  graph [-d <D>...] <log_names>...
+  graph [ --size=4000000] [ --save-csv==filename ] [ --from-csv=filename ] [ --port=8001 ]  [-d <D>...] <log_names>...
 
 Arguments:
   <log_names>        Address to read from
 
 Options:
   -d <D>        Device ID. Optional and repeatable. Default: current device
+  --size=<size>  Size of the buffer to read [default: 4000000]
+  --port=<port>  Port to serve the graph [default: 8001]
+  --save-csv=<filename>  Save the log data to a CSV file
+  --from-csv=<filename>  Read the log data from a CSV file
 
 Description:
   Reads and prints a block of data from address 'addr' at core <core-loc>.
@@ -17,6 +21,8 @@ Description:
 Examples:
   graph current
   graph -d 1 current power
+  graph current power --size 10000000
+  graph current power --save-csv log_data.csv
 """
 
 command_metadata = {
@@ -32,7 +38,7 @@ from ttlens.tt_uistate import UIState
 
 from ttlens.tt_lens_context import Context
 from typing import List
-from ttlens.tt_arc_dbg_fw_graph import arc_dfw_get_logs
+from ttlens.tt_arc_dbg_fw_graph import arc_dfw_get_logs, save_to_csv, read_logs_from_csv
 
 import plotly.express as px
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -40,30 +46,46 @@ from ttlens.tt_arc_dbg_fw_log_context import  LogInfo, ArcDfwLogContext, ArcDfwL
 import threading
 import os
 import socket
+from ttlens.tt_arc_dbg_fw import read_arc_dfw_buffer 
 
 def run(cmd_text, context, ui_state: UIState = None):
     args = docopt(command_metadata["description"], argv=cmd_text.split()[1:])
 
     log_names = args["<log_names>"]
+    size = int(args["--size"]) if args["--size"] else 4000000
+    port = int(args["--port"]) if args["--port"] else 8001
+    save_csv = args["--save-csv"] if args["--save-csv"] else None
+    from_csv = args["--from-csv"] if args["--from-csv"] else None
 
     devices = args["-d"]
     if devices:
         for device_id in devices:
-            graph(log_names,device_id, context)
+            graph(log_names, size, port, save_csv, from_csv, device_id, context)
     else:
-        graph(log_names, ui_state.current_device_id, context)
+        graph(log_names, size, port, save_csv, from_csv, ui_state.current_device_id, context)
 
-def graph(log_names: List[str], device_id: int, context: Context):
-
-
-    TT_METAL_ARC_DEBUG_BUFFER_SIZE=1024*64    
+def graph(log_names: List[str], size: int, port: int, save_csv: str, from_csv: str, device_id: int, context: Context):
+    TT_METAL_ARC_DEBUG_BUFFER_SIZE=size
     os.environ["TT_METAL_ARC_DEBUG_BUFFER_SIZE"] = str(TT_METAL_ARC_DEBUG_BUFFER_SIZE)
-    
-    log_context = ArcDfwLogContextFromList(log_names)
-    buffer_data = arc_dfw_get_logs(log_context, device_id, context)
+
+    buffer_data = {}
+    if from_csv:
+        buffer_data = read_logs_from_csv(from_csv)
+    else:
+        if log_names[0] == "all":
+            log_context = ArcDfwLogContextFromYaml("default")
+        else:
+            log_context = ArcDfwLogContextFromList(log_names)
+        buffer_data = arc_dfw_get_logs(log_context, device_id, context)
+
+    if save_csv:
+        save_to_csv(buffer_data, save_csv)
 
     figures = {}
     for key, data in buffer_data.items():
+        if key== "heartbeat" and "heartbeat" not in log_names:
+            continue
+            
         figures[key] = px.line(x=range(len(data)), y=data, title=key.capitalize())
 
     combined_html = "combined_plots.html"
@@ -74,14 +96,10 @@ def graph(log_names: List[str], device_id: int, context: Context):
             f.write(fig.to_html(full_html=False, include_plotlyjs='cdn' if key == list(figures.keys())[0] else False))
         f.write("</body></html>")
     
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-
     httpd = None
     
     def serve_html():
         nonlocal httpd
-        port = 8001
         os.chdir(".")  # Set the directory for the HTTP server
         httpd = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
         print(f"Graph address http://localhost:{port}/{combined_html} if port forwarding is set up.")
