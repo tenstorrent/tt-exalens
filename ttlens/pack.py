cbbe0335aab080fba2606a9fdfa7d77795a209a9
Aleksa Vuckovic <avuckovic@tenstorrent.com>
2024-12-30 21:27:49 +0000

    removing torch

--- ttlens/pack.py
+++ ttlens/pack.py
@@ -4,8 +4,6 @@
 # pack.py
 
 import struct
-import torch
-import struct
 
 
 def flatten_list(sublists):
@@ -18,8 +16,7 @@ def int_to_bytes_list(n):
 
 
 def float16_to_bytes(value):
-    float16_value = torch.tensor(value, dtype=torch.float16)
-    packed_bytes = struct.pack(">e", float16_value.item())
+    packed_bytes = struct.pack(">e", value)
     return list(packed_bytes) + [0] * (4 - len(packed_bytes))
 
 
@@ -40,40 +37,40 @@ def int32_to_bytes(number):
 
 
 def bfloat16_to_binary(value):
-    float_value = value.to(torch.float32).item()
+    float_value = float(value)
     bfloat16_bytes = bfloat16_to_bytes(float_value)
     return f"{bfloat16_bytes[0]:08b}{bfloat16_bytes[1]:08b}"
 
 
-def pack_bfp16(torch_tensor):
+def pack_bfp16(tensor):
     packed_bytes = []
-    for i in range(0, len(torch_tensor), 2):
-        half1 = bfloat16_to_bytes(torch_tensor[i])
-        half2 = bfloat16_to_bytes(torch_tensor[i + 1])
-        packed_bytes.extend([half1[0:2][::-1], half2[0:2][::-1]][::-1])  # reverse endian
+    for i in range(0, len(tensor), 2):
+        half1 = bfloat16_to_bytes(tensor[i])
+        half2 = bfloat16_to_bytes(tensor[i + 1])
+        packed_bytes.extend([half1[0:2][::-1], half2[0:2][::-1]][::-1])
     return flatten_list(packed_bytes)
 
 
-def pack_fp16(torch_tensor):
+def pack_fp16(tensor):
     packed_bytes = []
-    for i in range(0, len(torch_tensor), 2):
-        half1 = float16_to_bytes(torch_tensor[i])
-        half2 = float16_to_bytes(torch_tensor[i + 1])
-        packed_bytes.extend([half1[0:2][::-1], half2[0:2][::-1]][::-1])  # reverse endian
+    for i in range(0, len(tensor), 2):
+        half1 = float16_to_bytes(tensor[i])
+        half2 = float16_to_bytes(tensor[i + 1])
+        packed_bytes.extend([half1[0:2][::-1], half2[0:2][::-1]][::-1])
     return flatten_list(packed_bytes)
 
 
-def pack_fp32(torch_tensor):
+def pack_fp32(tensor):
     packed_bytes = []
-    for i in range(0, len(torch_tensor)):
-        packed_bytes.append(fp32_to_bytes(torch_tensor[i])[::-1])
+    for value in tensor:
+        packed_bytes.append(fp32_to_bytes(value)[::-1])
     return flatten_list(packed_bytes)
 
 
-def pack_int32(torch_tensor):
+def pack_int32(tensor):
     packed_bytes = []
-    for i in range(0, len(torch_tensor)):
-        packed_bytes.append(int32_to_bytes(torch_tensor[i])[::-1])
+    for value in tensor:
+        packed_bytes.append(int32_to_bytes(value)[::-1])
     return flatten_list(packed_bytes)
 
 
@@ -88,8 +85,8 @@ def float_to_bfp8_block(block):
         sign = binary_str[0]
         signs.append(int(sign, 2))
         exponent = int(binary_str[1:9], 2)
-        mantissa = binary_str[9:-1]  # remove last
-        mantissa = "1" + mantissa  ## add 1
+        mantissa = binary_str[9:-1]
+        mantissa = "1" + mantissa
         exponents.append(exponent)
         mantissas.append(mantissa)
         max_exponent = max(max_exponent, exponent)
@@ -109,7 +106,7 @@ def float_to_bfp8_block(block):
 
 
 def pack_bfp8_b(tensor, block_size=16):
-    flattened_tensor = tensor.flatten()
+    flattened_tensor = tensor
     num_blocks = len(flattened_tensor) // block_size
     blocks = [flattened_tensor[i * block_size : (i + 1) * block_size] for i in range(num_blocks)]
 
