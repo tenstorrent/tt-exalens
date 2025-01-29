0614448b7e9c1fa9cfd93701198da9749694ea41
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-01-29 10:32:48 +0000

    Added print_alu_config to cfg command

--- ttlens/ttlens_commands/dump-cfg-reg.py
+++ ttlens/ttlens_commands/dump-cfg-reg.py
@@ -33,6 +33,27 @@ from ttlens.tt_coordinate import OnChipCoordinate
 from ttlens.tt_debug_tensix import TensixDebug
 
 
+def print_alu_config_field(debug_tensix: TensixDebug, name: str):
+    print(f"{name}: 0x{debug_tensix.read_tensix_register(name)}")
+
+
+def print_alu_config(debug_tensix: TensixDebug):
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_Fpu_srnd_en")
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_Gasket_srnd_en")
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_Packer_srnd_en")
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_Padding")
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_GS_LF")
+    print_alu_config_field(debug_tensix, "ALU_ROUNDING_MODE_Bfp8_HF")
+    print_alu_config_field(debug_tensix, "ALU_FORMAT_SPEC_REG0_SrcAUnsigned")
+    print_alu_config_field(debug_tensix, "ALU_FORMAT_SPEC_REG0_SrcBUnsigned")
+    print_alu_config_field(debug_tensix, "ALU_FORMAT_SPEC_REG0_SrcA")
+    print_alu_config_field(debug_tensix, "ALU_FORMAT_SPEC_REG1_SrcB")
+    print_alu_config_field(debug_tensix, "ALU_FORMAT_SPEC_REG2_Dstacc")
+    print_alu_config_field(debug_tensix, "ALU_ACC_CTRL_Fp32_enabled")
+    print_alu_config_field(debug_tensix, "ALU_ACC_CTRL_SFPU_Fp32_enabled")
+    print_alu_config_field(debug_tensix, "ALU_ACC_CTRL_INT8_math_enabled")
+
+
 def run(cmd_text, context, ui_state: UIState = None):
     args = docopt(__doc__, argv=cmd_text.split()[1:])
 
@@ -50,7 +71,6 @@ def run(cmd_text, context, ui_state: UIState = None):
         core_loc = OnChipCoordinate.create(core_loc_str, device=current_device)
 
         debug_tensix = TensixDebug(core_loc, device_id, context)
-        data = debug_tensix.read_tensix_register("ALU_FORMAT_SPEC_REG2_Dstacc")
-        print(data)
+        print_alu_config(debug_tensix)
 
     return None
