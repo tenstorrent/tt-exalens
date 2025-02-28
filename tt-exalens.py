abeed08d12f21553583a1689c67f4d021282ef04
Milos Acimovic <macimovic@tenstorrent.com>
2025-02-28 10:21:00 +0000

    Renaming tt-lens.py to tt-exalens.py but adding symbolic link

---
+++ tt-exalens.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+# -*- coding: utf-8 -*-
+import re
+import sys
+from ttexalens.cli import main
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    sys.exit(main())
