13e0fb38ae72ae89b2c0fa5784c301635e70c966
Milos Acimovic <macimovic@tenstorrent.com>
2025-03-10 16:46:12 +0000

    Update output paths for noc overlay scripts in n2p.sh

--- scripts/noc_to_python/n2p.sh
+++ scripts/noc_to_python/n2p.sh
@@ -24,5 +24,5 @@ g++ -o ./scripts/noc_to_python/codegen-noc-overlay-blackhole \
     scripts/noc_to_python/noc_to_python.cpp \
     "$METAL_HW_INC"blackhole/noc/noc_overlay_parameters.cpp
 
-./scripts/noc_to_python/codegen-noc-overlay-wormhole > ./ttexalens/arch/wormhole/noc_overlay.py
-./scripts/noc_to_python/codegen-noc-overlay-blackhole > ./ttexalens/arch/blakchole/noc_overlay.py
+./scripts/noc_to_python/codegen-noc-overlay-wormhole > ./ttexalens/hw/tensix/wormhole/noc_overlay.py
+./scripts/noc_to_python/codegen-noc-overlay-blackhole > ./ttexalens/hw/tensix/blakchole/noc_overlay.py
