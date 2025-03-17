ceaa592b6814132f3c1a3fd5eba760ba3a8f78a6
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-03-17 11:32:00 +0000

    Completed dump and write reg commands

--- ttexalens/cli_commands/dump-reg.py
+++ ttexalens/cli_commands/dump-reg.py
@@ -3,7 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 """
 Usage:
-  dump-reg [ -d <device> ] [ -l <loc> ]
+  dump-reg <reg-type> <reg_parameters> [ -d <device> ] [ -l <loc> ]
+
+Arguments:
+  <reg-type>    Register type to dump. Options: [cfg, dbg]
+  <reg_parameters>  Register parameters. For cfg: index, mask, shift. For dbg: address
 
 Options:
   -d <device>   Device ID. Optional. Default: current device
@@ -13,10 +17,11 @@ Description:
   Prints the specified register, at the specified location and device.
 
 Examples:
-    dreg
-    dreg -d 0 -l 0,0
-    dreg -l 0,0
-    dreg -d 0
+    dreg cfg 60,0xf,0
+    dreg dbg 0x54
+    dreg dbg 0x54 -d 0 -l 0,0
+    dreg dbg 0x54 -l 0,0
+    dreg dbg 0x54 -d 0
 """
 
 command_metadata = {
@@ -29,8 +34,47 @@ command_metadata = {
 
 from ttexalens.uistate import UIState
 from ttexalens.debug_tensix import TensixDebug
-from ttexalens.device import Device
+from ttexalens.device import (
+    Device,
+    TensixRegisterDescription,
+    ConfigurationRegisterDescription,
+    DebugRegisterDescription,
+)
 from ttexalens import command_parser
+from ttexalens.util import TTException, INFO
+
+reg_types = ["cfg", "dbg"]
+
+
+def parse_reg_params(reg_params):
+    params = []
+    for param in reg_params.split(","):
+        try:
+            if param.startswith("0x"):
+                params.append(int(param, 16))
+            else:
+                params.append(int(param))
+        except ValueError:
+            raise ValueError("Invalid register parameter. Use integers separated by commas.")
+
+    return params
+
+
+def create_register_description(reg_type: str, reg_params: list[int]) -> TensixRegisterDescription:
+    if reg_type == "cfg":
+        if len(reg_params) != 3:
+            raise TTException(
+                "Invalid number of parameters for configuration register since it requires 3 parameters (index, mask, shift)."
+            )
+        return ConfigurationRegisterDescription(index=reg_params[0], mask=reg_params[1], shift=reg_params[2])
+    elif reg_type == "dbg":
+        if len(reg_params) != 1:
+            raise TTException(
+                "Invalid number of parameters for debug register since it requires 1 parameter (address)."
+            )
+        return DebugRegisterDescription(address=reg_params[0])
+    else:
+        raise ValueError(f"Unknown register type: {reg_type}. Possible values: {reg_types}")
 
 
 def run(cmd_text, context, ui_state: UIState = None):
@@ -39,6 +83,11 @@ def run(cmd_text, context, ui_state: UIState = None):
         argv=cmd_text.split()[1:],
     )
 
+    reg_type = dopt.args["<reg-type>"]
+    reg_params = parse_reg_params(dopt.args["<reg_parameters>"])
+
+    register = create_register_description(reg_type, reg_params)
+
     for device in dopt.for_each("--device", context, ui_state):
         for loc in dopt.for_each("--loc", context, ui_state, device=device):
-            pass
+            print(TensixDebug(loc, device.id(), context).read_tensix_register(register))
--- ttexalens/cli_commands/write-reg.py
+++ ttexalens/cli_commands/write-reg.py
@@ -3,20 +3,26 @@
 # SPDX-License-Identifier: Apache-2.0
 """
 Usage:
-  write-reg [ -d <device> ] [ -l <loc> ]
+  dump-reg <reg-type> <reg_parameters> <value> [ -d <device> ] [ -l <loc> ]
+
+Arguments:
+  <reg-type>        Register type to write to. Options: [cfg, dbg]
+  <reg_parameters>  Register parameters. For cfg: index, mask, shift. For dbg: address
+  <value>           Value to write to the register. Range: [0, 2^32-1]
 
 Options:
   -d <device>   Device ID. Optional. Default: current device
   -l <loc>      Core location in X-Y or R,C format. Default: current core
 
 Description:
-    Writes to the specified register, at the specified location and device.
+  Writes value to the specified register, at the specified location and device.
 
 Examples:
-    wreg
-    wreg -d 0 -l 0,0
-    wreg -l 0,0
-    wreg -d 0
+    wreg cfg 60,0xf,0 0
+    wreg dbg 0x54 0
+    wreg dbg 0x54 0 -d 0 -l 0,0
+    wreg dbg 0x54 0 -l 0,0
+    wreg dbg 0x54 0 -d 0
 """
 
 command_metadata = {
@@ -29,8 +35,47 @@ command_metadata = {
 
 from ttexalens.uistate import UIState
 from ttexalens.debug_tensix import TensixDebug
-from ttexalens.device import Device
+from ttexalens.device import (
+    Device,
+    TensixRegisterDescription,
+    ConfigurationRegisterDescription,
+    DebugRegisterDescription,
+)
 from ttexalens import command_parser
+from ttexalens.util import TTException, INFO
+
+reg_types = ["cfg", "dbg"]
+
+
+def parse_reg_params(reg_params):
+    params = []
+    for param in reg_params.split(","):
+        try:
+            if param.startswith("0x"):
+                params.append(int(param, 16))
+            else:
+                params.append(int(param))
+        except ValueError:
+            raise ValueError("Invalid register parameter. Use integers separated by commas.")
+
+    return params
+
+
+def create_register_description(reg_type: str, reg_params: list[int]) -> TensixRegisterDescription:
+    if reg_type == "cfg":
+        if len(reg_params) != 3:
+            raise TTException(
+                "Invalid number of parameters for configuration register since it requires 3 parameters (index, mask, shift)."
+            )
+        return ConfigurationRegisterDescription(index=reg_params[0], mask=reg_params[1], shift=reg_params[2])
+    elif reg_type == "dbg":
+        if len(reg_params) != 1:
+            raise TTException(
+                "Invalid number of parameters for debug register since it requires 1 parameter (address)."
+            )
+        return DebugRegisterDescription(address=reg_params[0])
+    else:
+        raise ValueError(f"Unknown register type: {reg_type}. Possible values: {reg_types}")
 
 
 def run(cmd_text, context, ui_state: UIState = None):
@@ -39,6 +84,12 @@ def run(cmd_text, context, ui_state: UIState = None):
         argv=cmd_text.split()[1:],
     )
 
+    reg_type = dopt.args["<reg-type>"]
+    reg_params = parse_reg_params(dopt.args["<reg_parameters>"])
+    value = int(dopt.args["<value>"])
+
+    register = create_register_description(reg_type, reg_params)
+
     for device in dopt.for_each("--device", context, ui_state):
         for loc in dopt.for_each("--loc", context, ui_state, device=device):
-            pass
+            TensixDebug(loc, device.id(), context).write_tensix_register(register, value)
