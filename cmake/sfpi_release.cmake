1e5ffd99ccb96039f3e53faf62fd83c713a8d974
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-04-03 17:11:24 +0000

    Remove unnecessary DOWNLOAD_EXTRACT_TIMESTAMP option in sfpi_release.cmake

--- cmake/sfpi_release.cmake
+++ cmake/sfpi_release.cmake
@@ -9,7 +9,6 @@ if(NOT EXISTS "${SFPI_RELEASE_PATH}")
         URL
             https://github.com/tenstorrent/sfpi/releases/download/v6.6.0/sfpi-release.tgz
         URL_HASH MD5=8eed4d1128809e2fb2df00e04b5a51ea
-        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
         SOURCE_DIR
         ${SFPI_RELEASE_PATH}
     )
