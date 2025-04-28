4bf8f0521b5d6010efebe6f60ea44368fddd648f
Ivan Hamer <ihamer@tenstorrent.com>
2025-04-28 05:39:09 +0000

    Update test-regdef.sh to include device detection and dynamic register definition loading
    - Changed file mode to executable.
    - Added a shebang for bash scripting.
    - Implemented device detection using lspci to differentiate between BLACKHOLE and WORMHOLE devices.
    - Set REGDEF variable based on detected device type before running unit tests.

--- test-regdef.sh
+++ test-regdef.sh
@@ -1,1 +1,28 @@
-REGDEF=../regdef/arc.json python3 -m unittest test/ttexalens/unit_tests/test_regdef_load.py
+#!/bin/bash
+
+# Use lspci to see if we have WH or BH device:
+# BLACKHOLE: [ 0 ] ~  > lspci -d 1e52:
+# 0c:00.0 Processing accelerators: Device 1e52:b140
+# WORMHOLE: [ 0 ] ~/work/tt-exalens  > lspci -d 1e52:
+# 31:00.0 Processing accelerators: Device 1e52:401e (rev 01)
+
+LSPCI=`lspci -d 1e52:`
+
+if [[ $LSPCI == *"b140"* ]]; then
+    DEV="BH"
+elif [[ $LSPCI == *"401e"* ]]; then
+    DEV="WH"
+fi
+
+if [ -z "$DEV" ]; then
+    echo "Unknown device"
+    exit 1
+fi
+
+if [ "$DEV" == "BH" ]; then
+    REGDEF=../regdef/arc.json
+elif [ "$DEV" == "WH" ]; then
+    REGDEF=../regdef/data/wormhole/axi-noc.yaml
+fi
+
+python3 -m unittest test/ttexalens/unit_tests/test_regdef_load.py
