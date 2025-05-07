0a9b3933c0c5f33a2faf8c6997b5c5245d76685d
Aleksandar Djordjevic <adjordjevic@tenstorrent.com>
2025-05-07 17:42:29 +0200

    Added support for firmware elfs and added object type to decode_symbols (#354)
    Added support for firmware elfs in read_elf by trying do find
    `.firmware_text` section if `.text` section is not found.
    
    Included object type in return of decode_symbols function.
    
    Also added test_elfs folder in test which contains elf files for testing
    purposes. For now that folder contains firmware for brisc.

--- ttexalens/parse_elf.py
+++ ttexalens/parse_elf.py
@@ -676,7 +676,7 @@ class FrameInfoProvider:
 
 
 def decode_symbols(elf_file):
-    functions = {}
+    symbols = {}
     for section in elf_file.iter_sections():
         # Check if it's a symbol table section
         if section.name == ".symtab":
@@ -684,10 +684,12 @@ def decode_symbols(elf_file):
             for symbol in section.iter_symbols():
                 # Check if it's a label symbol
                 if symbol["st_info"]["type"] == "STT_NOTYPE" and symbol.name:
-                    functions[symbol.name] = symbol["st_value"]
-                if symbol["st_info"]["type"] == "STT_FUNC":
-                    functions[symbol.name] = symbol["st_value"]
-    return functions
+                    symbols[symbol.name] = symbol["st_value"]
+                elif symbol["st_info"]["type"] == "STT_FUNC":
+                    symbols[symbol.name] = symbol["st_value"]
+                elif symbol["st_info"]["type"] == "STT_OBJECT":
+                    symbols[symbol.name] = symbol["st_value"]
+    return symbols
 
 
 def parse_dwarf(dwarf: DWARFInfo, loaded_offset=0):
@@ -740,7 +742,16 @@ def read_elf(file_ifc, elf_file_path, load_address=None):
     if not elf.has_dwarf_info():
         print(f"ERROR: {elf_file_path} does not have DWARF info. Source file must be compiled with -g")
         return
-    text_sh_address = elf.get_section_by_name(".text")["sh_addr"]
+
+    text_sh = elf.get_section_by_name(".text")
+    if text_sh is None:
+        text_sh = elf.get_section_by_name(".firmware_text")
+    if text_sh is None:
+        print(f"ERROR: Could not locate text section in {elf_file_path}.")
+        return
+
+    text_sh_address = text_sh["sh_addr"]
+
     if load_address is None:
         load_address = text_sh_address
 
