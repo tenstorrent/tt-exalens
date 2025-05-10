194cb3928d04525e3d45c5aa0666264ae88d2bea
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-05-10 11:24:44 +0000

    Expose noc_id in read/write API

--- test/ttexalens/server/test_python_server.cpp
+++ test/ttexalens/server/test_python_server.cpp
@@ -28,40 +28,41 @@ class simulation_server : public tt::exalens::server {
 // For every write combination, read of the same communication will return that result.
 class simulation_implementation : public tt::exalens::ttexalens_implementation {
    private:
-    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint64_t>, uint32_t> read_write_4;
-    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint64_t, uint32_t>, std::vector<uint8_t>> read_write;
+    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t, uint64_t>, uint32_t> read_write_4;
+    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t, uint64_t, uint32_t>, std::vector<uint8_t>> read_write;
     std::map<std::tuple<uint8_t, uint64_t>, uint32_t> read_write_4_raw;
-    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint64_t>, uint32_t> jtag_read_write_4;
-    std::map<uint32_t, uint32_t> jtag_read_write_2;
+    std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t, uint64_t>, uint32_t> jtag_read_write_4;
+    std::map<uint32_t, uint32_t> jtag_read_write_4_axi;
 
    protected:
-    std::optional<uint32_t> pci_read32(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address) override {
-        auto it = read_write_4.find(std::make_tuple(chip_id, noc_x, noc_y, address));
+    std::optional<uint32_t> pci_read32(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y,
+                                       uint64_t address) override {
+        auto it = read_write_4.find(std::make_tuple(noc_id, chip_id, noc_x, noc_y, address));
         if (it != read_write_4.end()) {
             return it->second;
         }
         return {};
     }
-    std::optional<uint32_t> pci_write32(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
+    std::optional<uint32_t> pci_write32(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
                                         uint32_t data) override {
-        read_write_4[std::make_tuple(chip_id, noc_x, noc_y, address)] = data;
+        read_write_4[std::make_tuple(noc_id, chip_id, noc_x, noc_y, address)] = data;
         return 4;
     }
-    std::optional<std::vector<uint8_t>> pci_read(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
-                                                 uint32_t size) override {
-        auto it = read_write.find(std::make_tuple(chip_id, noc_x, noc_y, address, size));
+    std::optional<std::vector<uint8_t>> pci_read(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y,
+                                                 uint64_t address, uint32_t size) override {
+        auto it = read_write.find(std::make_tuple(noc_id, chip_id, noc_x, noc_y, address, size));
         if (it != read_write.end()) {
             return it->second;
         }
         return {};
     }
-    std::optional<uint32_t> pci_write(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
+    std::optional<uint32_t> pci_write(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
                                       const uint8_t* data, uint32_t size) override {
         std::vector<uint8_t> data_vector(size);
         for (size_t i = 0; i < size; i++) {
             data_vector[i] = data[i];
         }
-        read_write[std::make_tuple(chip_id, noc_x, noc_y, address, size)] = data_vector;
+        read_write[std::make_tuple(noc_id, chip_id, noc_x, noc_y, address, size)] = data_vector;
         return size;
     }
     std::optional<uint32_t> pci_read32_raw(uint8_t chip_id, uint64_t address) override {
@@ -83,36 +84,37 @@ class simulation_implementation : public tt::exalens::ttexalens_implementation {
         return {};
     }
 
-    std::optional<uint32_t> jtag_read32(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address) override {
-        auto it = jtag_read_write_4.find(std::make_tuple(chip_id, noc_x, noc_y, address));
+    std::optional<uint32_t> jtag_read32(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y,
+                                        uint64_t address) override {
+        auto it = jtag_read_write_4.find(std::make_tuple(noc_id, chip_id, noc_x, noc_y, address));
         if (it != jtag_read_write_4.end()) {
             return it->second;
         }
         return {};
     }
-    std::optional<int> jtag_write32(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
+    std::optional<int> jtag_write32(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
                                     uint32_t data) override {
-        jtag_read_write_4[std::make_tuple(chip_id, noc_x, noc_y, address)] = data;
+        jtag_read_write_4[std::make_tuple(noc_id, chip_id, noc_x, noc_y, address)] = data;
         return 4;
     }
 
     std::optional<uint32_t> jtag_read32_axi(uint8_t chip_id, uint32_t address) override {
-        auto it = jtag_read_write_2.find(address);
-        if (it != jtag_read_write_2.end()) {
+        auto it = jtag_read_write_4_axi.find(address);
+        if (it != jtag_read_write_4_axi.end()) {
             return it->second;
         }
         return {};
     }
     std::optional<int> jtag_write32_axi(uint8_t chip_id, uint32_t address, uint32_t data) override {
-        jtag_read_write_2[address] = data;
+        jtag_read_write_4_axi[address] = data;
         return 4;
     }
 
-    std::optional<std::string> pci_read_tile(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y, uint64_t address,
-                                             uint32_t size, uint8_t data_format) override {
-        return "pci_read_tile(" + std::to_string(chip_id) + ", " + std::to_string(noc_x) + ", " +
-               std::to_string(noc_y) + ", " + std::to_string(address) + ", " + std::to_string(size) + ", " +
-               std::to_string(data_format) + ")";
+    std::optional<std::string> pci_read_tile(uint8_t noc_id, uint8_t chip_id, uint8_t noc_x, uint8_t noc_y,
+                                             uint64_t address, uint32_t size, uint8_t data_format) override {
+        return "pci_read_tile(" + std::to_string(noc_id) + ", " + std::to_string(chip_id) + ", " +
+               std::to_string(noc_x) + ", " + std::to_string(noc_y) + ", " + std::to_string(address) + ", " +
+               std::to_string(size) + ", " + std::to_string(data_format) + ")";
     }
     std::optional<std::string> get_cluster_description() override { return "get_cluster_description()"; }
     std::optional<std::vector<uint8_t>> get_device_ids() override { return std::vector<uint8_t>{0, 1}; }
