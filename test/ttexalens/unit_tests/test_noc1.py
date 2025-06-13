d315a4080dcbdd7fc0371e41e6427980692ec16f
Ivan Klikovac <iklikovac@tenstorrent.com>
2025-06-13 09:45:01 +0000

    Completed

--- test/ttexalens/unit_tests/test_noc1.py
+++ test/ttexalens/unit_tests/test_noc1.py
@@ -11,21 +11,21 @@ class TestNOC1(unittest.TestCase):
     def setUp(self):
         self.context = init_test_context() # Use NOC1
         self.device = self.context.devices[0]
-        self.loc = OnChipCoordinate(0, 0, "logical", self.device)
-        self.register_store = self.device.get_block(self.loc).get_register_store(1) # NOC1 register store
+        self.loc = OnChipCoordinate(1, 0, "logical", self.device, core_type="tensix")
+        self.noc0_register_store = self.device.get_block(self.loc).get_register_store(0)
+        self.noc1_register_store = self.device.get_block(self.loc).get_register_store(1)
 
     def test_noc1(self):
-        # int write_words_to_device("0,0", 0x333, 111, noc_id=1)
+        # Read NOC0 and NOC1
+        noc0_wr_req_before = self.noc0_register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        noc1_wr_req_before = self.noc1_register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        # Write through NOC1
+        write_words_to_device(self.loc, 0x333, [1, 1, 1, 3], noc_id=1)
+        # Read again
+        noc0_wr_req_after = self.noc0_register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        noc1_wr_req_after = self.noc1_register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
 
-        prev1 = self.register_store.read_register("NIU_MST_NONPOSTED_WR_REQ_SENT")
-        prev2 = self.register_store.read_register("NIU_MST_POSTED_WR_REQ_SENT")
-        prev3 = self.register_store.read_register("NIU_MST_WR_ACK_RECEIVED")
-        prev4 = self.register_store.read_register("NIU_MST_RD_REQ_SENT")
-
-        prev5 = self.register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
-        prev6 = self.register_store.read_register("NIU_SLV_POSTED_WR_REQ_RECEIVED")
-        prev7 = self.register_store.read_register("NIU_SLV_WR_ACK_SENT")
-        prev8 = self.register_store.read_register("NIU_SLV_RD_REQ_RECEIVED")
-
-        write_words_to_device(self.loc, 0x333, 1, noc_id=1)
-        self.assertLess(prev1, self.register_store.read_register("NIU_MST_NONPOSTED_WR_REQ_SENT"))
+        # NOC1 counter should've increased by at least 8
+        self.assertGreaterEqual(noc1_wr_req_after, noc1_wr_req_before + 8)
+        # The difference on NOC1 should be strictly greater than the difference on NOC0
+        self.assertGreater(noc1_wr_req_after - noc1_wr_req_before, noc0_wr_req_after - noc0_wr_req_before)
