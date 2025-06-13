477a960bfc48d64f4e76ce12f4c5a26fc44fc787
Ivan Klikovac <iklikovac@tenstorrent.com>
2025-06-13 13:45:52 +0000

    minor cleanup

--- test/ttexalens/unit_tests/test_noc.py
+++ test/ttexalens/unit_tests/test_noc.py
@@ -25,10 +25,11 @@ class TestNOC(unittest.TestCase):
         self.context = init_test_context(self.use_noc1)
         self.device = self.context.devices[0]
         self.loc = OnChipCoordinate(1, 0, "logical", self.device, core_type="tensix")
-        self.rs = self.device.get_block(self.loc).get_register_store(self.noc_id)
+        self.register_store = self.device.get_block(self.loc).get_register_store(self.noc_id)
 
     def test_noc_write(self):
-        before = self.rs.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        # Read the counter, write 3 values through the NOC, and expect the counter to go up by at least 6
+        before = self.register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
         write_words_to_device(self.loc, 0x333, [1, 1, 1], noc_id=self.noc_id)
-        after = self.rs.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        after = self.register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
         self.assertGreaterEqual(after, before + 6)
