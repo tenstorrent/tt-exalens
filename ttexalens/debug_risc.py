1dc0b9384d89240e364fa57bf8e871d4368ca8f3
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-06-14 11:07:36 +0000

    Remove deprecated elf_loaded call in RiscLoader with a TODO for future API migration

--- ttexalens/debug_risc.py
+++ ttexalens/debug_risc.py
@@ -945,7 +945,8 @@ class RiscLoader:
             util.ERROR(e)
             raise util.TTException(f"Error loading elf file {elf_path}")
 
-        self.context.elf_loaded(self.risc_debug.location.loc, self.risc_debug.location.risc_id, elf_path)
+        # TODO: This will be removed with PR that moves RiscLoader to new API
+        # self.context.elf_loaded(self.risc_debug.location.loc, self.risc_debug.location.risc_id, elf_path)
         return init_section_address
 
     def load_elf(self, elf_path: str):
