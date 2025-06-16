70b696d7f7ab6ee1c9f4b4e6c0ceae4b00f910d0
Ivan Klikovac <iklikovac@tenstorrent.com>
2025-06-16 13:14:51 +0200

    Add NOC tests (#445)
    #280 
    Added `init_test_context` into `test_base.py` that starts a TTExaLens
    context on either NOC0 or NOC1.
    Added `test_noc.py` that checks if the NOCs behave consistently
    regardless of which one hosts the context.
    The test sets up two contexts (NOC0 and NOC1), and for each context:
    - reads the transaction counters,
    - performs differently-sized writes through NOC0 and NOC1,
    - and checks that each NOC counter increased by at least the amount
    corresponding to the data sent through it.
    Two out of four tests (ones where NOC1 hosts the context) are currently disabled due to issue #455.

--- test/ttexalens/unit_tests/test_base.py
+++ test/ttexalens/unit_tests/test_base.py
@@ -12,3 +12,11 @@ def init_default_test_context():
         return tt_exalens_init.init_ttexalens_remote(ip_address, port)
     else:
         return tt_exalens_init.init_ttexalens()
+
+
+def init_test_context(use_noc1: bool = False):
+    if use_noc1:
+        assert not os.getenv("TTEXALENS_TESTS_REMOTE"), "Remote testing for NOC1 not supported"
+        return tt_exalens_init.init_ttexalens(use_noc1=True)
+    else:
+        return init_default_test_context()
---
+++ test/ttexalens/unit_tests/test_noc.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import unittest
+from parameterized import parameterized_class
+from test.ttexalens.unit_tests.test_base import init_test_context
+from ttexalens.coordinate import OnChipCoordinate
+from ttexalens.tt_exalens_lib import write_words_to_device
+
+
+@parameterized_class(
+    [
+        {"use_noc1": False, "noc_id": 0},
+        {"use_noc1": False, "noc_id": 1},
+        # Disabled until issue #455 is resolved
+        # {"use_noc1": True, "noc_id": 0},
+        # {"use_noc1": True, "noc_id": 1},
+    ]
+)
+class TestNOC(unittest.TestCase):
+    use_noc1: bool
+    noc_id: int
+
+    def setUp(self):
+        self.context = init_test_context(self.use_noc1)
+        self.device = self.context.devices[0]
+        self.loc = OnChipCoordinate(1, 0, "logical", self.device, core_type="tensix")
+        self.register_store = self.device.get_block(self.loc).get_register_store(self.noc_id)
+
+    def test_noc_write(self):
+        # Read the counter, write 3 values through the NOC, and expect the counter to go up by at least 6
+        before = self.register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        write_words_to_device(self.loc, 0x333, [1, 1, 1], noc_id=self.noc_id)
+        after = self.register_store.read_register("NIU_SLV_NONPOSTED_WR_REQ_RECEIVED")
+        self.assertGreaterEqual(after, before + 6)
