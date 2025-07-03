ae61b6ebf6034c9ca8f91624ed66a31c12f26b40
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-03 14:54:51 +0000

    fix

--- ttexalens/hardware/baby_risc_debug.py
+++ ttexalens/hardware/baby_risc_debug.py
@@ -414,7 +414,7 @@ class BabyRiscDebugHardware:
             return None
 
     def get_pc(self) -> int:
-        debug_bus_pc_signal: DebugBusSignalDescription = self.debug_bus_pc_signal
+        debug_bus_pc_signal = self.debug_bus_pc_signal()
         if debug_bus_pc_signal is not None:
             pc = self.risc_info.noc_block.debug_bus.read_signal(debug_bus_pc_signal)
             if self.risc_info.risc_name == "ncrisc" and pc & 0xF0000000 == 0x70000000:
