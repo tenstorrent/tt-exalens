423c4dfbd97d064b70e0c344e343f19e96533aa1
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-08 13:05:42 +0000

    Updating gdb script

--- auto_gdb.sh
+++ auto_gdb.sh
@@ -11,36 +11,60 @@ PID=$2
 ELF=$3
 OFFSET=$4
 
-# Start the GDB server in the background
-#./tt-exalens.py --start-gdb=$PORT
+[ -p mypipe ] || mkfifo mypipe
+tail -f mypipe | ./tt-exalens.py --start-gdb=$PORT > exalens_log.txt &
 
-# # Wait until port is open
-# echo "Waiting for GDB server on port $PORT..."
-# while ! nc -z localhost "$PORT"; do
-#     sleep 0.2
-# done
-# echo "GDB server ready."
+sleep 1 # Wait for the GDB server to start
 
 # Generate a temporary GDB script
 GDB_SCRIPT=$(mktemp /tmp/gdbscript.XXXX.gdb)
 
 cat > "$GDB_SCRIPT" <<EOF
+# Make script run without user interaction
 set pagination off
 set confirm off
-set logging file gdb.output
+
+# Logging for callstack results
+set logging file callstack.output
+shell > callstack.output
 
 target extended-remote localhost:$PORT
-attach $PID
-add-symbol-file $ELF $OFFSET
+python
+try:
+    gdb.execute("attach $PID")
+except gdb.error as e:
+    if "failed" in str(e):
+        print("Failed to attach to process. Aborting...\nTry resseting the board with tt-smi.")
+    else:
+        print(e)
+end
+
+python
+try:
+    gdb.execute("add-symbol-file $ELF $OFFSET")
+except gdb.error as e:
+    print(e)
+end
+
 set logging enabled on
 bt
 set logging enabled off
-detach
+
+python
+try:
+    gdb.execute("detach")
+except gdb.error as e:
+    print("Detach failed:", e)
+end
+
 q
 EOF
 
 # Run GDB with the generated script
-./build/sfpi/compiler/bin/riscv32-tt-elf-gdb -x "$GDB_SCRIPT"
+./build/sfpi/compiler/bin/riscv32-tt-elf-gdb -q -batch -x "$GDB_SCRIPT" > tmp.txt
+
+# Close port
+sudo kill -9 $(sudo lsof -t -i :$PORT)
 
 # Clean up
 rm "$GDB_SCRIPT"
