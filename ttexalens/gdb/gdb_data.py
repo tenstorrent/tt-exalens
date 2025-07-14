33a819d5673177b305c4b579cd560aa368f6ec13
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-14 12:20:58 +0000

    Fixing bug that causes attach to target multiple processes

--- ttexalens/gdb/gdb_data.py
+++ ttexalens/gdb/gdb_data.py
@@ -12,7 +12,7 @@ class GdbThreadId:
     thread_id: int
 
     def to_gdb_string(self):
-        return f"p{self.process_id}.{self.thread_id}"
+        return f"p{hex(self.process_id)[2:]}.{hex(self.thread_id)[2:]}"
 
 
 @dataclass
