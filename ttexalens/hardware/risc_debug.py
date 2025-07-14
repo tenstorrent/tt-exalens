bd0263b50055b332364d971b6d360b26dcc46e71
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-14 11:12:49 +0000

    Precommit

--- ttexalens/hardware/risc_debug.py
+++ ttexalens/hardware/risc_debug.py
@@ -73,7 +73,7 @@ class RiscDebug:
     def get_instance(risc_location: RiscLocation) -> "RiscDebug":
         noc_block = risc_location.location._device.get_block(risc_location.location)
         return noc_block.get_risc_debug(risc_location.risc_name, risc_location.neo_id)
-    
+
     @staticmethod
     def get_block_type(risc_location: RiscLocation) -> str:
         noc_block = risc_location.location._device.get_block(risc_location.location)
