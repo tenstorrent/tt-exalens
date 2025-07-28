03a67100d8986c48e174c2c298f3ad8106d2768d
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-28 14:20:33 +0000

    test workaround

--- ttexalens/hardware/blackhole/baby_risc_debug.py
+++ ttexalens/hardware/blackhole/baby_risc_debug.py
@@ -21,3 +21,18 @@ class BlackholeBabyRiscDebug(BabyRiscDebug):
         else:
             assert self.noc_block.debug_bus is not None, "Debug bus is not initialized."
             return self.noc_block.debug_bus.read_signal(self.risc_info.risc_name + "_pc")
+
+    def read_memory(self, address: int):
+        if self.enable_asserts:
+            self.assert_not_in_reset()
+        self.assert_debug_hardware()
+        assert self.debug_hardware is not None, "Debug hardware is not initialized"
+        bytes_shifted = address % 4
+        if bytes_shifted == 0:  # aligned read
+            return self.debug_hardware.read_memory(address)
+        else:
+            A = self.debug_hardware.read_memory(address)
+            B = self.debug_hardware.read_memory(address + 4)
+            mask_A = 1 << ((4 - bytes_shifted) * 8) - 1
+            mask_B = bytes_shifted * 8
+            return ((A & mask_A) << 8 * bytes_shifted) | (B >> (32 - mask_B))
