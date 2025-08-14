00dbf5c6e5b8406aba3aa0e3bb1e34680e5a06e6
Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>
2025-08-14 16:48:18 +0200

    Porting quasar simulator support to new code (#521)
    With new way of running simulation, we don't need run simulation test script.
    Adding lots of fixes in order to unblock Quasar.

--- test/ttexalens/unit_tests/test_risc_debug.py
+++ test/ttexalens/unit_tests/test_risc_debug.py
@@ -14,24 +14,39 @@ from ttexalens.elf_loader import ElfLoader
 
 @parameterized_class(
     [
-        {"core_desc": "ETH0", "risc_name": "ERISC"},
-        {"core_desc": "ETH0", "risc_name": "ERISC0"},
-        {"core_desc": "ETH0", "risc_name": "ERISC1"},
-        {"core_desc": "FW0", "risc_name": "BRISC"},
-        {"core_desc": "FW0", "risc_name": "TRISC0"},
-        {"core_desc": "FW0", "risc_name": "TRISC1"},
-        {"core_desc": "FW0", "risc_name": "TRISC2"},
-        {"core_desc": "FW0", "risc_name": "TRISC3"},
-        {"core_desc": "FW1", "risc_name": "BRISC"},
-        {"core_desc": "FW1", "risc_name": "TRISC0"},
-        {"core_desc": "FW1", "risc_name": "TRISC1"},
-        {"core_desc": "FW1", "risc_name": "TRISC2"},
-        {"core_desc": "FW1", "risc_name": "TRISC3"},
-        # {"core_desc": "DRAM0", "risc_name": "DRISC"},
+        # {"core_desc": "ETH0", "risc_name": "ERISC", "neo_id": None},
+        # {"core_desc": "ETH0", "risc_name": "ERISC0", "neo_id": None},
+        # {"core_desc": "ETH0", "risc_name": "ERISC1", "neo_id": None},
+        {"core_desc": "FW0", "risc_name": "BRISC", "neo_id": None},
+        {"core_desc": "FW0", "risc_name": "TRISC0", "neo_id": None},
+        {"core_desc": "FW0", "risc_name": "TRISC1", "neo_id": None},
+        {"core_desc": "FW0", "risc_name": "TRISC2", "neo_id": None},
+        {"core_desc": "FW1", "risc_name": "BRISC", "neo_id": None},
+        {"core_desc": "FW1", "risc_name": "TRISC0", "neo_id": None},
+        {"core_desc": "FW1", "risc_name": "TRISC1", "neo_id": None},
+        {"core_desc": "FW1", "risc_name": "TRISC2", "neo_id": None},
+        # {"core_desc": "DRAM0", "risc_name": "DRISC", "neo_id": None},
+        {"core_desc": "FW0", "risc_name": "TRISC0", "neo_id": 0},
+        {"core_desc": "FW0", "risc_name": "TRISC1", "neo_id": 0},
+        {"core_desc": "FW0", "risc_name": "TRISC2", "neo_id": 0},
+        {"core_desc": "FW0", "risc_name": "TRISC3", "neo_id": 0},
+        {"core_desc": "FW0", "risc_name": "TRISC0", "neo_id": 1},
+        {"core_desc": "FW0", "risc_name": "TRISC1", "neo_id": 1},
+        {"core_desc": "FW0", "risc_name": "TRISC2", "neo_id": 1},
+        {"core_desc": "FW0", "risc_name": "TRISC3", "neo_id": 1},
+        {"core_desc": "FW0", "risc_name": "TRISC0", "neo_id": 2},
+        {"core_desc": "FW0", "risc_name": "TRISC1", "neo_id": 2},
+        {"core_desc": "FW0", "risc_name": "TRISC2", "neo_id": 2},
+        {"core_desc": "FW0", "risc_name": "TRISC3", "neo_id": 2},
+        {"core_desc": "FW0", "risc_name": "TRISC0", "neo_id": 3},
+        {"core_desc": "FW0", "risc_name": "TRISC1", "neo_id": 3},
+        {"core_desc": "FW0", "risc_name": "TRISC2", "neo_id": 3},
+        {"core_desc": "FW0", "risc_name": "TRISC3", "neo_id": 3},
     ]
 )
 class TestDebugging(unittest.TestCase):
     risc_name: str  # Risc name
+    neo_id: int | None  # NEO ID
     context: Context  # TTExaLens context
     core_desc: str  # Core description ETH0, FW0, FW1 - being parametrized
     core_sim: RiscvCoreSimulator  # RISC-V core simulator instance
@@ -42,12 +57,20 @@ class TestDebugging(unittest.TestCase):
 
     def setUp(self):
         try:
-            self.core_sim = RiscvCoreSimulator(self.context, self.core_desc, self.risc_name)
+            self.core_sim = RiscvCoreSimulator(self.context, self.core_desc, self.risc_name, self.neo_id)
         except ValueError as e:
             if self.risc_name.lower() in e.__str__().lower():
                 self.skipTest(f"Core {self.risc_name} not available on this platform: {e}")
+            elif "ETH core" in e.__str__() or "FW core" in e.__str__():
+                self.skipTest(f"Core {self.core_desc}:{self.risc_name} not available on this platform: {e}")
             else:
                 raise e
+        except AssertionError as e:
+            if self.neo_id is not None and "NEO ID" in e.__str__():
+                self.skipTest(f"Test requires NEO ID, but is not supported on this platform: {e}")
+            else:
+                raise e
+
         self.device = self.context.devices[0]
 
         # Stop risc with reset
@@ -76,6 +99,9 @@ class TestDebugging(unittest.TestCase):
         )
 
     def test_default_start_address(self):
+        if self.core_sim.is_quasar():
+            self.skipTest("Skipping Quasar test since it lasts for 1 hour on simulator.")
+
         risc_info = self.core_sim.risc_debug.risc_info
 
         if risc_info.default_code_start_address is None:
@@ -168,7 +194,14 @@ class TestDebugging(unittest.TestCase):
 
     def test_read_write_private_memory(self):
         """Testing read_memory and write_memory through debugging interface on private core memory range."""
-        addr = 0xFFB00000
+        data_private = self.core_sim.risc_debug.get_data_private_memory()
+        self.assertIsNotNone(data_private, "Data private memory should not be None.")
+        assert data_private is not None
+        self.assertIsNotNone(data_private.address.private_address, "Private address should not be None.")
+        assert data_private.address.private_address is not None
+
+        addr = data_private.address.private_address
+        noc_addr = data_private.address.noc_address
 
         # Write code for brisc core at address 0
         # C++:
@@ -187,11 +220,26 @@ class TestDebugging(unittest.TestCase):
         # Value should not be changed and should stay the same since core is in halt
         self.assertTrue(self.core_sim.is_halted(), "Core should be halted.")
 
+        # Test read if noc address exists
+        if noc_addr is not None:
+            self.core_sim.write_data_checked(noc_addr, 0xAABBCCDD)
+            self.assertEqual(
+                self.core_sim.risc_debug.read_memory(addr), 0xAABBCCDD, "Memory value should be 0xaabbccdd."
+            )
+
         # Test write and read memory
         self.core_sim.risc_debug.write_memory(addr, 0x12345678)
         self.assertEqual(self.core_sim.risc_debug.read_memory(addr), 0x12345678, "Memory value should be 0x12345678.")
+        if noc_addr is not None:
+            self.assertEqual(
+                self.core_sim.read_data(noc_addr), 0x12345678, "Memory value read over NOC should be 0x12345678."
+            )
         self.core_sim.risc_debug.write_memory(addr, 0x87654321)
         self.assertEqual(self.core_sim.risc_debug.read_memory(addr), 0x87654321, "Memory value should be 0x87654321.")
+        if noc_addr is not None:
+            self.assertEqual(
+                self.core_sim.read_data(noc_addr), 0x87654321, "Memory value read over NOC should be 0x87654321."
+            )
 
     def test_minimal_run_generated_code(self):
         """Test running 16 bytes of generated code that just write data on memory and does infinite loop. All that is done on brisc."""
@@ -477,7 +525,7 @@ class TestDebugging(unittest.TestCase):
         self.assertFalse(self.core_sim.is_ebreak_hit(), "ebreak should not be the cause.")
 
     def test_invalidate_cache(self):
-        if not self.core_sim.is_blackhole():
+        if self.core_sim.is_wormhole():
             self.skipTest("Invalidate cache is not reliable on wormhole.")
 
         if self.core_sim.is_eth_block():
@@ -535,9 +583,9 @@ class TestDebugging(unittest.TestCase):
 
         # Halt to verify PC
         self.core_sim.halt()
-        self.assertTrue(self.core_sim.is_halted(), "Core should not be halted.")
+        self.assertTrue(self.core_sim.is_halted(), "Core should be halted.")
 
-        # There is hardware bug on blackhole that causes PC to be 0
+        # There is hardware bug on blackhole that causes PC to be 0, but we added a fix to read pc using debug bus
         self.assertPcEquals(12)
 
         # Verify value at address
@@ -626,13 +674,17 @@ class TestDebugging(unittest.TestCase):
         #   asm volatile ("nop");
         #  jump_addr:
         #   goto start;
-        for i in range(jump_addr // 4):
-            self.core_sim.write_program(i * 4, 0x00000013)
+        self.core_sim.write_program(0, [0x00000013] * (jump_addr // 4))
         self.core_sim.write_program(break_addr, 0x00100073)
         self.core_sim.write_program(jump_addr, ElfLoader.get_jump_to_offset_instruction(-jump_addr))
 
         # Take risc out of reset
         self.core_sim.set_reset(False)
+
+        # Since simulator is slow, we need to wait a bit by reading something
+        if self.core_sim.is_quasar():
+            for i in range(50):
+                self.core_sim.read_data(0)
 
         # Value should not be changed and should stay the same since core is in halt
         self.assertEqual(self.core_sim.read_data(addr), 0x12345678)
@@ -658,6 +710,12 @@ class TestDebugging(unittest.TestCase):
         # Continue execution
         self.core_sim.continue_execution()
         self.assertFalse(self.core_sim.is_halted(), "Core should not be halted.")
+
+        # Since simulator is slow, we need to wait a bit by reading something
+        if self.core_sim.is_quasar():
+            for i in range(200):
+                if self.core_sim.read_data(addr) == 0x87654000:
+                    break
 
         # Halt to verify PC
         self.core_sim.halt()
@@ -1150,6 +1208,9 @@ class TestDebugging(unittest.TestCase):
         if self.core_sim.is_blackhole():
             self.skipTest("BNE instruction with debug hardware enabled is fixed in blackhole.")
 
+        if self.core_sim.is_quasar():
+            self.skipTest("BNE instruction with debug hardware enabled is fixed in quasar.")
+
         # Enable branch prediction
         self.core_sim.set_branch_prediction(True)
 
@@ -1272,6 +1333,11 @@ class TestDebugging(unittest.TestCase):
         # Continue to proceed with bne test
         self.core_sim.debug_hardware.continue_without_debug()  # We need to use debug hardware as there is a bug fix in risc debug implementation for wormhole
 
+        # Since simulator is slow, we need to wait a bit by reading something
+        if self.core_sim.is_quasar():
+            for i in range(20):
+                self.core_sim.read_data(0)
+
         # We should pass for loop very fast and should be halted here already
         self.assertTrue(self.core_sim.is_halted(), "Core should be halted.")
         self.assertTrue(self.core_sim.is_ebreak_hit(), "ebreak should be the cause.")
@@ -1349,6 +1415,11 @@ class TestDebugging(unittest.TestCase):
 
         # Continue to proceed with bne test
         self.core_sim.debug_hardware.cont()  # We need to use debug hardware as there is a bug fix in risc debug implementation for wormhole
+
+        # Since simulator is slow, we need to wait a bit by reading something
+        if self.core_sim.is_quasar():
+            for i in range(20):
+                self.core_sim.read_data(0)
 
         # We should pass for loop very fast and should be halted here already
         self.assertTrue(self.core_sim.is_halted(), "Core should be halted.")
