0ac586ac3169091163ef51ec5b875b9ff32f5b23
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-10-07 14:10:04 +0000

    Fixing close device

--- ttexalens/pybind/inc/open_implementation.h
+++ ttexalens/pybind/inc/open_implementation.h
@@ -22,6 +22,7 @@ class open_implementation : public BaseClass {
     std::vector<uint8_t> device_ids;
     std::map<uint8_t, std::string> device_soc_descriptors_yamls;
     std::map<uint8_t, tt_SocDescriptor> soc_descriptors;
+    bool is_simulation = false;
 
     std::string cluster_descriptor_path;
 
@@ -42,6 +43,7 @@ class open_implementation : public BaseClass {
     std::optional<std::tuple<uint8_t, uint8_t>> convert_from_noc0(uint8_t chip_id, uint8_t noc_x, uint8_t noc_y,
                                                                   const std::string& core_type,
                                                                   const std::string& coord_system) override;
+    std::optional<std::tuple<uint64_t, uint64_t, uint64_t>> get_firmware_version(uint8_t chip_id) override;
 };
 
 }  // namespace tt::exalens
