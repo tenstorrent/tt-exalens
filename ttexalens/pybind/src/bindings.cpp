7f7a181a53d0dc6e0f014ce38f908bda3a7bdbc6
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2025-10-30 12:26:43 +0000

    Change timeout type to std::chrono::milliseconds

--- ttexalens/pybind/src/bindings.cpp
+++ ttexalens/pybind/src/bindings.cpp
@@ -94,7 +94,7 @@ class TTExaLensImplementation {
     }
 
     std::tuple<int, uint32_t, uint32_t> arc_msg(uint8_t noc_id, uint8_t chip_id, uint32_t msg_code, bool wait_for_done,
-                                                uint32_t arg0, uint32_t arg1, int timeout) {
+                                                uint32_t arg0, uint32_t arg1, std::chrono::milliseconds timeout) {
         return _check_result(implementation->arc_msg(noc_id, chip_id, msg_code, wait_for_done, arg0, arg1, timeout));
     }
 
