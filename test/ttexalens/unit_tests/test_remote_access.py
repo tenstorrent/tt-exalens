3134fd41564b2c969ca75b10309601fcf641a29b
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-11-07 14:09:47 +0000

    Move remote_devices to setUpClass

--- test/ttexalens/unit_tests/test_remote_access.py
+++ test/ttexalens/unit_tests/test_remote_access.py
@@ -20,15 +20,15 @@ class TestRemoteAccess(unittest.TestCase):
     def setUpClass(cls):
         cls.context = init_default_test_context()
         cls.local_device = cls.context.devices[0]
+        cls.remote_devices = [cls.context.devices[i] for i in range(1, len(cls.context.devices))]
 
     def test_remote_access(self):
-        if len(self.context.devices) < 2:
+
+        if len(self.remote_devices) == 0:
             self.skipTest("There are no remote devices to test")
 
-        remote_devices = [self.context.devices[i] for i in range(1, len(self.context.devices))]
-
         fw_versions1: list[FirmwareVersion] = []
-        for remote_device in remote_devices:
+        for remote_device in self.remote_devices:
             fw_versions1.append(FirmwareVersion(self.context.server_ifc.get_firmware_version(remote_device._id)))
             eth_core = self.context.server_ifc.get_currently_active_eth_core(remote_device._id)
             coord_str = f"e{eth_core[0]},{eth_core[1]}"
@@ -41,8 +41,8 @@ class TestRemoteAccess(unittest.TestCase):
         self.context = init_default_test_context()
 
         fw_versions2: list[FirmwareVersion] = []
-        for remote_device in remote_devices:
+        for remote_device in self.remote_devices:
             fw_versions2.append(FirmwareVersion(self.context.server_ifc.get_firmware_version(remote_device._id)))
 
-        for i in range(len(remote_devices)):
+        for i in range(len(self.remote_devices)):
             self.assertEqual(fw_versions1[i], fw_versions2[i])
