12d3f2e745b10c56cdf2b530c2275dd95a549c84
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-11-10 16:38:53 +0000

    Doing warm_reset only if test involved ETH core

--- test/ttexalens/unit_tests/test_remote_communication.py
+++ test/ttexalens/unit_tests/test_remote_communication.py
@@ -7,28 +7,34 @@ from test.ttexalens.unit_tests.test_base import init_default_test_context
 from ttexalens.context import Context
 from ttexalens.coordinate import OnChipCoordinate
 from ttexalens.device import Device
-from ttexalens.util import FirmwareVersion
+from ttexalens.tt_exalens_lib import read_word_from_device, write_words_to_device
 
 
 class TestRemoteCommunication(unittest.TestCase):
     context: Context  # TTExaLens context
     local_device: Device  # Local (PCIE) device
     remote_devices: list[Device]  # Remote devices
+    tensix_core: OnChipCoordinate
 
     @classmethod
     def setUpClass(cls):
         cls.context = init_default_test_context()
         cls.local_device = cls.context.devices[0]
         cls.remote_devices = [cls.context.devices[i] for i in range(1, len(cls.context.devices))]
+        cls.tensix_core = OnChipCoordinate.create("0,0", cls.local_device)
 
     def test_remote_communication(self):
+
+        data = 0x12345678
+        address = 0x100
 
         if len(self.remote_devices) == 0:
             self.skipTest("There are no remote devices to test")
 
-        fw_versions1: list[FirmwareVersion] = []
         for remote_device in self.remote_devices:
-            fw_versions1.append(FirmwareVersion(self.context.server_ifc.get_firmware_version(remote_device._id)))
+            write_words_to_device(self.tensix_core, address, data, remote_device._id)
+            ret = read_word_from_device(self.tensix_core, address, remote_device._id)
+            self.assertEqual(ret, data)
             eth_core = self.context.server_ifc.get_remote_transfer_eth_core(remote_device._id)
             coord_str = f"e{eth_core[0]},{eth_core[1]}"
             loc = OnChipCoordinate.create(coord_str, self.local_device)
@@ -36,12 +42,10 @@ class TestRemoteCommunication(unittest.TestCase):
             risc_debug = noc_block.get_default_risc_debug()
             risc_debug.halt()
 
-        fw_versions2: list[FirmwareVersion] = []
         for remote_device in self.remote_devices:
-            fw_versions2.append(FirmwareVersion(self.context.server_ifc.get_firmware_version(remote_device._id)))
-
-        for i in range(len(self.remote_devices)):
-            self.assertEqual(fw_versions1[i], fw_versions2[i])
+            write_words_to_device(self.tensix_core, address, data, remote_device._id)
+            ret = read_word_from_device(self.tensix_core, address, remote_device._id)
+            self.assertEqual(ret, data)
 
     def tearDown(self) -> None:
         self.context.server_ifc.warm_reset()
