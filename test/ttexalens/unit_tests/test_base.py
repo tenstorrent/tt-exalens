86540ecfb7c1947096f67f86783d9c9a4f8fd797
Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>
2025-11-12 23:34:20 +0100

    Adding tests for riscv l1 memory writes (#694)
    Adding tests that verify riscv L1 write behavior.
    Adding RiscvProgramWriter that helps writing simple programs easily.
    Replacing old `self.core_sim.write_program` with using new program
    writer.
    Extracting `test_debug_bus.py` out of `test_risc_debug.py`

--- test/ttexalens/unit_tests/test_base.py
+++ test/ttexalens/unit_tests/test_base.py
@@ -4,6 +4,9 @@
 from ttexalens import tt_exalens_init
 import os
 
+from ttexalens.coordinate import OnChipCoordinate
+from ttexalens.device import Device
+
 # Global cache for simulator context to ensure only one simulator process
 # is created when TTEXALENS_SIMULATOR is set, preventing conflicts between
 # parameterized test classes
@@ -33,3 +36,31 @@ def init_test_context(use_noc1: bool = False):
         return tt_exalens_init.init_ttexalens(use_noc1=True)
     else:
         return init_default_test_context()
+
+
+def get_core_location(core_desc: str, device: Device) -> OnChipCoordinate:
+    """Convert core_desc to core location string."""
+    if core_desc.startswith("ETH"):
+        eth_blocks = device.idle_eth_blocks
+        core_index = int(core_desc[3:])
+        if len(eth_blocks) > core_index:
+            return eth_blocks[len(eth_blocks) - 1 - core_index].location
+        raise ValueError(f"ETH core {core_index} not available on this platform")
+
+    elif core_desc.startswith("FW"):
+        fw_cores = device.get_block_locations(block_type="functional_workers")
+        core_index = int(core_desc[2:])
+        if len(fw_cores) > core_index:
+            return fw_cores[core_index]
+        raise ValueError(f"FW core {core_index} not available on this platform")
+    elif core_desc.startswith("DRAM"):
+        dram_cores = device.get_block_locations(block_type="dram")
+        core_index = int(core_desc[4:])
+        if len(dram_cores) > core_index:
+            return dram_cores[core_index]
+        raise ValueError(f"DRAM core {core_index} not available on this platform")
+
+    try:
+        return OnChipCoordinate.create(core_desc, device=device)
+    except:
+        raise ValueError(f"Unknown core description {core_desc}")
