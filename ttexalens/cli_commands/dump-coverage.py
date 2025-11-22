6cd17a6dc384178f59472ee111dfbbef8b1e27c7
Aleksandar Djordjevic <adjordjevic@tenstorrent.com>
2025-11-22 16:26:32 +0100

    Fixing documentation generation, adding pre-requisites for dump-coverage, updating docs (#716)
    Closes #714 
    
    Added `run-elf` as pre-requisite for `dump-coverage` effectively fixing
    `make docs`. Removed coverage for callstack test on `trisc0` as it
    killed host. Made an
    [issue](https://github.com/tenstorrent/tt-exalens/issues/718) for this.
    Updated documentation.

--- ttexalens/cli_commands/dump-coverage.py
+++ ttexalens/cli_commands/dump-coverage.py
@@ -17,7 +17,7 @@ Description:
   and place it into the output directory along with its gcno.
 
 Examples:
-  cov build/riscv-src/wormhole/callstack.coverage.trisc0.elf coverage/callstack.gcda
+  re build/riscv-src/wormhole/cov_test.coverage.brisc.elf -r brisc # Pre-requisite: we have to run the elf before running coverage
   cov build/riscv-src/wormhole/cov_test.coverage.brisc.elf coverage/cov_test.gcda coverage/cov_test.gcno
 """
 
