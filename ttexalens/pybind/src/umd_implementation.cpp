tenstorrent/tt-exalens

commit 9920873432da2a71126c5c2321a8806ada9d25ef

Lowering timeout of read/write reg and checking if read value is 0xffffffff (#735)
Lowered default timeout from 500 to 200 ms in `read_from_device_reg` and
`write_to_device_reg` to support blackhole since same invalid read
scenario takes about 220 ms.

Also added check if read value is `0xffffffff` before throwing
exception.

ttexalens/pybind/src/umd_implementation.cpp
@@ -43,12 +43,13 @@ class TimeoutDeviceRegisterException : public std::exception {
 
 void read_from_device_reg(tt::umd::Cluster* cluster, void* temp, uint8_t chip_id, tt::umd::CoreCoord tensix_core,
                           uint64_t addr, uint32_t size,
-                          std::chrono::milliseconds timeout = std::chrono::milliseconds(500)) {
+                          std::chrono::milliseconds timeout = std::chrono::milliseconds(200)) {
     auto start_time = std::chrono::steady_clock::now();
     cluster->read_from_device_reg(temp, chip_id, tensix_core, addr, size);
     auto end_time = std::chrono::steady_clock::now();
     auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    if (elapsed_time > timeout) {
+    // Address will always be aligned to 4 bytes, so we can check the last 4 bytes for 0xFFFFFFFF
+    if (elapsed_time > timeout && *((uint32_t*)temp + size - 4) == 0xFFFFFFFF) {
         tensix_core = cluster->get_soc_descriptor(chip_id).translate_coord_to(tensix_core, CoordSystem::LOGICAL);
         throw TimeoutDeviceRegisterException(chip_id, tensix_core, addr, size, true);
     }
@@ -56,7 +57,7 @@ void read_from_device_reg(tt::umd::Cluster* cluster, void* temp, uint8_t chip_id
 
 void write_to_device_reg(tt::umd::Cluster* cluster, const void* temp, uint32_t size, uint8_t chip_id,
                          tt::umd::CoreCoord tensix_core, uint64_t addr,
-                         std::chrono::milliseconds timeout = std::chrono::milliseconds(500)) {
+                         std::chrono::milliseconds timeout = std::chrono::milliseconds(200)) {
     auto start_time = std::chrono::steady_clock::now();
     cluster->write_to_device_reg(temp, size, chip_id, tensix_core, addr);
     auto end_time = std::chrono::steady_clock::now();

commit 831c33cd17f63ad587617f273f8b22fb14127de1

Fixing bug when calculating address of last word (#738)
Bug was introduced in https://github.com/tenstorrent/tt-exalens/pull/735
when calculating address of last word. This PR fixes that.

ttexalens/pybind/src/umd_implementation.cpp
@@ -49,7 +49,7 @@ void read_from_device_reg(tt::umd::Cluster* cluster, void* temp, uint8_t chip_id
     auto end_time = std::chrono::steady_clock::now();
     auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
     // Address will always be aligned to 4 bytes, so we can check the last 4 bytes for 0xFFFFFFFF
-    if (elapsed_time > timeout && *((uint32_t*)temp + size - 4) == 0xFFFFFFFF) {
+    if (elapsed_time > timeout && *((uint32_t*)temp + size / 4 - 1) == 0xFFFFFFFF) {
         tensix_core = cluster->get_soc_descriptor(chip_id).translate_coord_to(tensix_core, CoordSystem::LOGICAL);
         throw TimeoutDeviceRegisterException(chip_id, tensix_core, addr, size, true);
     }