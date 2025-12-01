5d8854ee0c2e437704a5a4f252e982fabef7a763
Bojan Rosko <brosko@tenstorrent.com>
2025-12-01 12:17:05 +0000

    simplify

--- ttexalens/tt_exalens_ifc.py
+++ ttexalens/tt_exalens_ifc.py
@@ -105,108 +105,52 @@ try:
             simulation_directory: str | None = None,
         ):
             self.temp_working_directory = tempfile.mkdtemp(prefix="ttexalens_server_XXXXXX")
+
+            # // Disable UMD logging
+            # tt::umd::logging::set_level(tt::umd::logging::level::error);
+
+            # TODO: Hack on UMD on how to use/initialize with noc1. This should be removed once we have a proper way to use noc1
+            tt_umd.TTDevice.use_noc1(initialize_with_noc1)
+
+            device_type = tt_umd.IODeviceType.PCIe if not init_jtag else tt_umd.IODeviceType.JTAG
+            self.cluster_descriptor, self.devices = tt_umd.TopologyDiscovery.discover()
+
+            if len(self.cluster_descriptor.get_all_chips()) == 0:
+                raise RuntimeError("No Tenstorrent devices were detected on this system.")
+
             self.cluster_descriptor_path = os.path.join(self.temp_working_directory, "cluster_desc.yaml")
             self.device_soc_descriptors_yamls: dict[int, str] = {}
             self.devices: dict[int, UmdDeviceWrapper] = {}
             self.device_ids: list[int] = []
 
-            if simulation_directory is not None:
-                tt_umd.logging.set_level(tt_umd.logging.Level.Debug)
-                tt_device = tt_umd.RtlSimulationTTDevice.create(simulation_directory)
-                soc_descriptor = tt_device.get_soc_descriptor()
-                if tt_device.get_arch() == tt_umd.ARCH.BLACKHOLE:
-                    # Fix for old VSC Blackhole simulator
-                    for core in soc_descriptor.get_cores(tt_umd.CoreType.TENSIX):
-                        core_noc0 = soc_descriptor.translate_coord_to(core, tt_umd.CoordSystem.NOC0)
-                        tt_device.noc_write32(core_noc0.x, core_noc0.y, 0, 0x6F)
-                        tt_device.send_tensix_risc_reset(tt_umd.tt_xy_pair(core.x, core.y), deassert=True)
-                self.devices[0] = UmdDeviceWrapper(tt_device, 0, 0, soc_descriptor=soc_descriptor, is_simulation=True)
-                self.device_ids.append(0)
-                with open(self.cluster_descriptor_path, "w") as f:
-                    f.write(f"arch: {{\n")
-                    f.write(f"   0: {self.devices[0].arch},\n")
-                    f.write(f"}}\n\n")
-                    f.write(f"chips: {{\n")
-                    f.write(f"   0: [0,0,0,0],\n")
-                    f.write(f"}}\n\n")
-                    f.write(f"ethernet_connections: [\n")
-                    f.write(f"]\n\n")
-                    f.write(f"chips_with_mmio: [\n")
-                    f.write(f"   0: 0,\n")
-                    f.write(f"]\n\n")
-                    f.write(
-                        f"# harvest_mask is the bit indicating which tensix row is harvested. So bit 0 = first tensix row; bit 1 = second tensix row etc...\n"
-                    )
-                    f.write(f"harvesting: {{\n")
-                    f.write(f"   0: {{noc_translation: false, harvest_mask: 0}},\n")
-                    f.write(f"}}\n\n")
-                    f.write(
-                        f"# This value will be null if the boardtype is unknown, should never happen in practice but to be defensive it would be useful to throw an error on this case.\n"
-                    )
-                    f.write(f"boardtype: {{\n")
-                    f.write(f"   0: {self.devices[0].arch}Simulator,\n")
-                    f.write(f"}}\n")
-                    f.write(f"io_device_type: SIMULATION\n")
-                # TODO: In destructor we need to call close device so that emulation can stop reservation and simulation can stop waveform?!?
-            else:
-                # Respect UMD's existing env var first; default to ERROR otherwise.
-                # If Python wants DEBUG, it can set TT_LOGGER_LEVEL=debug before calling into this function.
-                if "TT_LOGGER_LEVEL" not in os.environ:
-                    tt_umd.logging.set_level(tt_umd.logging.Level.Error)
+            # Setup used devices
+            self.device_ids = []
+            target_devices = set()
 
-                # TODO: Hack on UMD on how to use/initialize with noc1. This should be removed once we have a proper way to use noc1
-                tt_umd.TTDevice.use_noc1(initialize_with_noc1)
+            for i in self.cluster_descriptor.get_all_chips():
+                self.device_ids.append(i)
 
-                discovery_options = tt_umd.TopologyDiscoveryOptions()
-                discovery_options.io_device_type = (
-                    tt_umd.IODeviceType.PCIe if not init_jtag else tt_umd.IODeviceType.JTAG
-                )
-                # TODO: discovery_options.no_wait_for_eth_training = True
-                # TODO: discovery_options.no_eth_firmware_strictness = True
-                self.cluster_descriptor, devices = tt_umd.TopologyDiscovery.discover(discovery_options)
+            self.device_id_to_unique_id = {}
+            unique_ids = self.cluster_descriptor.get_chip_unique_ids()
+            for device_id in self.device_ids:
+                if device_id in unique_ids:
+                    self.device_id_to_unique_id[device_id] = unique_ids[device_id]
 
-                if len(self.cluster_descriptor.get_all_chips()) == 0:
-                    raise RuntimeError("No Tenstorrent devices were detected on this system.")
-                self.cluster_descriptor.serialize_to_file(self.cluster_descriptor_path)
+            # If we specified which devices we want, check that they are available and then extract their ids
+            if len(wanted_devices) > 0:
+                for wanted_device in wanted_devices:
+                    if wanted_device not in self.device_ids:
+                        raise RuntimeError(f"Device {wanted_device} is not available.")
+                self.device_ids = wanted_devices
 
-                # Setup used devices
-                for i in self.cluster_descriptor.get_all_chips():
-                    self.device_ids.append(i)
+            for device_id in self.device_ids:
+                target_devices.add(device_id)
+            self.soc_descriptors = {}
+            self.device_soc_descriptors_yamls = {}
+            self.cached_arc_telemetry_readers = {}
+            for chip_id, device in self.devices.items():
 
-                device_id_to_unique_id = {}
-                unique_ids = self.cluster_descriptor.get_chip_unique_ids()
-                for device_id in self.device_ids:
-                    if device_id in unique_ids:
-                        device_id_to_unique_id[device_id] = unique_ids[device_id]
-
-                # If we specified which devices we want, check that they are available and then extract their ids
-                if len(wanted_devices) > 0:
-                    for wanted_device in wanted_devices:
-                        if wanted_device not in self.device_ids:
-                            raise RuntimeError(f"Device {wanted_device} is not available.")
-                    self.device_ids = wanted_devices
-
-                for chip_id in self.device_ids:
-                    device = devices[chip_id]
-                    unique_id = device_id_to_unique_id.get(chip_id, None)
-                    assert unique_id is not None, f"Unique ID for device {chip_id} not found."
-
-                    if not self.cluster_descriptor.is_chip_mmio_capable(chip_id):
-                        soc_descriptor = tt_umd.SocDescriptor(device)
-                        mmio_chip_id = self.cluster_descriptor.get_closest_mmio_capable_chip(chip_id)
-                        active_eth_channels = self.cluster_descriptor.get_active_eth_channels(mmio_chip_id)
-                        active_eth_cores = soc_descriptor.get_eth_cores_for_channels(
-                            active_eth_channels, tt_umd.CoordSystem.TRANSLATED
-                        )
-                        active_eth_coords_on_mmio_chip = [(core.x, core.y) for core in active_eth_cores]
-                    else:
-                        active_eth_coords_on_mmio_chip = []
-
-                    wrapped_device = UmdDeviceWrapper(device, chip_id, unique_id, active_eth_coords_on_mmio_chip)
-                    assert wrapped_device.is_mmio_capable == self.cluster_descriptor.is_chip_mmio_capable(chip_id)
-                    self.devices[chip_id] = wrapped_device
-
-            for chip_id, wrapped_device in self.devices.items():
+                soc_descriptor = tt_umd.SocDescriptor(device)
                 file_name = os.path.join(self.temp_working_directory, f"device_desc_runtime_{chip_id}.yaml")
                 wrapped_device.soc_descriptor.serialize_to_file(file_name)
                 self.device_soc_descriptors_yamls[chip_id] = file_name
