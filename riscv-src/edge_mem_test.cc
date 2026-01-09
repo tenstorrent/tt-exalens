e9e0655bc1e555f5fb5f421f29be5343c6826550
Ognjen Nenezic <onenezic@tenstorrent.com>
2026-01-09 21:38:50 +0100

    Protect reading/writing memory through GdbServer (#819)
    Closes #785 
    
    This PR adds a restriction to gdb's reads and writes (`x/X/m/M`) to only
    L1 and DataPrivateMemory. We will not allow partial reads and especially
    not partial writes - gdb supports partial reads on some high level
    commands like `print` (you ask for X bytes, but return Y) which in
    return shows us a `$<incomplete type>`, at lower level it's probably an
    error that the higher level transforms into that incomplete type,
    sometimes it transforms and can potentially put is in some weird and
    undefined states with gdb. We have a new error code `E04` will signify
    this being a memory access error - if a lower level command like x/X/m/M
    tried a restricted read it will raise it, for a higher level command
    like `print` that error will be raised but transformed into
    `$<incomplete type>`.
    
    Also added a bunch of tests that test the server against just `x/X/m/M`
    as well as just spawn our gdb client and test it as an integration test.

---
+++ riscv-src/edge_mem_test.cc
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+// SPDX-License-Identifier: Apache-2.0
+
+#include <array>
+#include <cstdint>
+
+volatile std::array<std::uint8_t, 16>* g_p_wormhole =
+    reinterpret_cast<volatile std::array<std::uint8_t, 16>*>(0x0016DFF8);  // 8 bytes in L1, 8 bytes past L1
+
+volatile std::array<std::uint8_t, 16>* g_p_blackhole =
+    reinterpret_cast<volatile std::array<std::uint8_t, 16>*>(0x0017FFF8);  // 8 bytes in L1, 8 bytes past L1
+
+volatile std::array<std::uint8_t, 16>* g_in_l1_wormhole =
+    reinterpret_cast<volatile std::array<std::uint8_t, 16>*>(0x00010000);  // entirely in L1
+
+volatile std::array<std::uint8_t, 16>* g_in_l1_blackhole =
+    reinterpret_cast<volatile std::array<std::uint8_t, 16>*>(0x00010000);  // entirely in L1
+
+std::array<std::uint8_t, 16> g_data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+int main() {
+    while (true) {
+        asm volatile("nop");
+    }
+    return 0;
+}
