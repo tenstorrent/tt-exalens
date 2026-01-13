4e3231497476e05d142aa072d1f4e6862da96a3a
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2026-01-13 09:31:57 +0000

    unsafe-best-match

--- .github/Dockerfile.ci.sh
+++ .github/Dockerfile.ci.sh
@@ -19,7 +19,7 @@ rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED
 pip install --upgrade pip --ignore-installed
 
 # Install Python dependencies
-uv pip install --no-cache-dir --system --extra-index-url https://test.pypi.org/simple/ -r requirements.txt
+uv pip install --no-cache-dir --system --index-strategy unsafe-best-match --extra-index-url https://test.pypi.org/simple/ -r requirements.txt
 uv pip install --no-cache-dir --system -r dev-requirements.txt
 uv pip install --no-cache-dir --system -r test_requirements.txt
 uv pip install --no-cache-dir --system wheel build setuptools
