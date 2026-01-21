5a122b93c632a7c032477305db7daf82165ce675
Aleksandar Djordjevic <adjordjevic@tenstorrent.com>
2026-01-21 08:41:09 +0100

    Update ttexalens/hardware/blackhole/dram_block.py
    Co-authored-by: Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>

--- ttexalens/hardware/blackhole/dram_block.py
+++ ttexalens/hardware/blackhole/dram_block.py
@@ -182,6 +182,34 @@ class BlackholeDramBlockSim(BlackholeNocBlock):
     def __init__(self, location: OnChipCoordinate):
         super().__init__(location, block_type="dram")
 
+        self.dram_bank = MemoryBlock(
+            size=4 * 1024 * 1024 * 1024,
+            address=DeviceAddress(noc_address=0x00000000),
+        )
+        self.noc0_regs = MemoryBlock(
+            size=0x10000,
+            address=DeviceAddress(noc_address=0x100FFB20000),
+        )
+        self.noc1_regs = MemoryBlock(
+            size=0x10000,
+            address=DeviceAddress(noc_address=0x100FFB30000),
+        )
+        self.noc_overlay = MemoryBlock(
+            size=0x10000,
+            address=DeviceAddress(noc_address=0x100FFB40000),
+        )
+
+        self.register_store_noc0 = RegisterStore(register_store_noc0_initialization, self.location)
+        self.register_store_noc1 = RegisterStore(register_store_noc1_initialization, self.location)
+
+        self.noc_memory_map.add_blocks(
+            [
+                MemoryMapBlockInfo("dram_bank", self.dram_bank),
+                MemoryMapBlockInfo("noc0_regs", self.noc0_regs.just_noc_address()),
+                MemoryMapBlockInfo("noc1_regs", self.noc1_regs.just_noc_address()),
+                MemoryMapBlockInfo("noc_overlay", self.noc_overlay.just_noc_address()),
+            ]
+        )
 
 class BlackholeDramBlock(BlackholeNocBlock):
     def __init__(self, location: OnChipCoordinate):
