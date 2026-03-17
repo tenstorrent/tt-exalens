ddbe015291034e529cba5ed5ec4f5284301b3e8d
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2026-03-17 10:16:50 +0000

    Add NOC memory blocks to WH blocks

--- ttexalens/hardware/wormhole/router_only_block.py
+++ ttexalens/hardware/wormhole/router_only_block.py
@@ -3,9 +3,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from ttexalens.coordinate import OnChipCoordinate
+from ttexalens.hardware.device_address import DeviceAddress
+from ttexalens.hardware.memory_block import MemoryBlock
 from ttexalens.hardware.wormhole.noc_block import WormholeNocBlock
+from ttexalens.memory_map import MemoryMapBlockInfo
 
 
 class WormholeRouterOnlyBlock(WormholeNocBlock):
     def __init__(self, location: OnChipCoordinate):
         super().__init__(location, block_type="router_only")
+
+        self.noc_regs = MemoryBlock(
+            size=0x10000, address=DeviceAddress(private_address=0xFFB20000, noc_address=0xFFB20000)
+        )
+        self.noc_memory_map.add_blocks(
+            [
+                MemoryMapBlockInfo("noc_regs", self.noc_regs),
+            ]
+        )
