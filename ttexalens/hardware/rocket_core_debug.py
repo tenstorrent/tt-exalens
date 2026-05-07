49d42e729dfa9501b917780dac943bd076fe99bd
adjordjevic-TT <adjordjevic@tenstorrent.com>
2026-05-07 15:55:18 +0000

    Overriding abstract methods in RocketCoreDebug

--- ttexalens/hardware/rocket_core_debug.py
+++ ttexalens/hardware/rocket_core_debug.py
@@ -6,7 +6,13 @@ from contextlib import contextmanager
 from typing import Any, Generator
 
 from ttexalens.hardware.baby_risc_info import BabyRiscInfo
-from ttexalens.hardware.risc_debug import RiscDebug, RiscLocation
+from ttexalens.hardware.memory_block import MemoryBlock
+from ttexalens.hardware.risc_debug import (
+    RiscDebug,
+    RiscDebugStatus,
+    RiscDebugWatchpointState,
+    RiscLocation,
+)
 
 
 class RocketCoreDebug(RiscDebug):
@@ -32,8 +38,14 @@ class RocketCoreDebug(RiscDebug):
     def is_halted(self) -> bool:
         raise NotImplementedError("is_halted must be implemented by subclasses of RocketCoreDebug")
 
+    def is_ebreak_hit(self) -> bool:
+        raise NotImplementedError("is_ebreak_hit must be implemented by subclasses of RocketCoreDebug")
+
     def halt(self) -> None:
         raise NotImplementedError("halt must be implemented by subclasses of RocketCoreDebug")
+
+    def step(self) -> None:
+        raise NotImplementedError("step must be implemented by subclasses of RocketCoreDebug")
 
     def cont(self) -> None:
         raise NotImplementedError("cont must be implemented by subclasses of RocketCoreDebug")
@@ -42,8 +54,71 @@ class RocketCoreDebug(RiscDebug):
     def ensure_halted(self) -> Generator[None, Any, None]:
         raise NotImplementedError("ensure_halted must be implemented by subclasses of RocketCoreDebug")
 
+    @contextmanager
+    def ensure_private_memory_access(self) -> Generator[None, Any, None]:
+        raise NotImplementedError("ensure_private_memory_access must be implemented by subclasses of RocketCoreDebug")
+
+    def read_gpr(self, register_index: int) -> int:
+        raise NotImplementedError("read_gpr must be implemented by subclasses of RocketCoreDebug")
+
+    def write_gpr(self, register_index: int, value: int) -> None:
+        raise NotImplementedError("write_gpr must be implemented by subclasses of RocketCoreDebug")
+
     def get_pc(self) -> int:
         raise NotImplementedError("get_pc must be implemented by subclasses of RocketCoreDebug")
 
+    def read_memory(self, address: int, safe_mode: bool | None = None) -> int:
+        raise NotImplementedError("read_memory must be implemented by subclasses of RocketCoreDebug")
+
+    def read_memory_bytes(self, address: int, size_bytes: int, safe_mode: bool | None = None) -> bytes:
+        raise NotImplementedError("read_memory_bytes must be implemented by subclasses of RocketCoreDebug")
+
+    def write_memory(self, address: int, data: int, safe_mode: bool | None = None) -> None:
+        raise NotImplementedError("write_memory must be implemented by subclasses of RocketCoreDebug")
+
+    def write_memory_bytes(self, address: int, data: bytes, safe_mode: bool | None = None) -> None:
+        raise NotImplementedError("write_memory_bytes must be implemented by subclasses of RocketCoreDebug")
+
+    def read_status(self) -> RiscDebugStatus:
+        raise NotImplementedError("read_status must be implemented by subclasses of RocketCoreDebug")
+
+    def read_watchpoints_state(self) -> list[RiscDebugWatchpointState]:
+        raise NotImplementedError("read_watchpoints_state must be implemented by subclasses of RocketCoreDebug")
+
+    def read_watchpoint_address(self, watchpoint_index: int) -> int:
+        raise NotImplementedError("read_watchpoint_address must be implemented by subclasses of RocketCoreDebug")
+
+    def disable_watchpoint(self, watchpoint_index: int) -> None:
+        raise NotImplementedError("disable_watchpoint must be implemented by subclasses of RocketCoreDebug")
+
+    def set_watchpoint_on_pc_address(self, watchpoint_index: int, address: int) -> None:
+        raise NotImplementedError("set_watchpoint_on_pc_address must be implemented by subclasses of RocketCoreDebug")
+
+    def set_watchpoint_on_memory_read(self, watchpoint_index: int, address: int) -> None:
+        raise NotImplementedError("set_watchpoint_on_memory_read must be implemented by subclasses of RocketCoreDebug")
+
+    def set_watchpoint_on_memory_write(self, watchpoint_index: int, address: int) -> None:
+        raise NotImplementedError("set_watchpoint_on_memory_write must be implemented by subclasses of RocketCoreDebug")
+
+    def set_watchpoint_on_memory_access(self, watchpoint_index: int, address: int) -> None:
+        raise NotImplementedError(
+            "set_watchpoint_on_memory_access must be implemented by subclasses of RocketCoreDebug"
+        )
+
+    def set_branch_prediction(self, enable: bool) -> None:
+        raise NotImplementedError("set_branch_prediction must be implemented by subclasses of RocketCoreDebug")
+
+    def can_debug(self) -> bool:
+        raise NotImplementedError("can_debug must be implemented by subclasses of RocketCoreDebug")
+
     def set_code_start_address(self, address: int | None) -> None:
         raise NotImplementedError("set_code_start_address must be implemented by subclasses of RocketCoreDebug")
+
+    def get_l1(self) -> MemoryBlock:
+        raise NotImplementedError("get_l1 must be implemented by subclasses of RocketCoreDebug")
+
+    def get_data_private_memory(self) -> MemoryBlock | None:
+        raise NotImplementedError("get_data_private_memory must be implemented by subclasses of RocketCoreDebug")
+
+    def get_code_private_memory(self) -> MemoryBlock | None:
+        raise NotImplementedError("get_code_private_memory must be implemented by subclasses of RocketCoreDebug")
