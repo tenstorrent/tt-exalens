bd44d37fc8338b6840a2f0b8281379d76d8c0206
adjordjevic-TT <adjordjevic@tenstorrent.com>
2026-09-21 15:19:50 +0000

    Add is_supported function to multiple command modules for enhanced command validation

--- ttexalens/cli_commands/dump-tensix-state.py
+++ ttexalens/cli_commands/dump-tensix-state.py
@@ -52,6 +52,12 @@ from ttexalens.util import (
 )
 from ttexalens.command_parser import CommandMetadata, tt_docopt, CommonCommandOptions
 
+
+def is_supported(device: Device, location: OnChipCoordinate, neo_id: int | None = None) -> bool:
+    print(device.get_block_type(location))
+    return device.get_block_type(location) == "functional_workers"
+
+
 command_metadata = CommandMetadata(
     short_name="tensix",
     type="low-level",
@@ -62,6 +68,7 @@ command_metadata = CommandMetadata(
         CommonCommandOptions.Verbose,
         CommonCommandOptions.Neo,
     ],
+    is_supported=is_supported,
 )
 
 possible_groups = ["all", "alu", "pack", "unpack", "gpr", "rwc", "adc"]
